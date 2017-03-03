cfda823d61159c8180a38aa50c397713b90e7d19
platania <platania@research.att.com>
2017-03-03 17:42:03 -0500

    generate high traffic in vFW
    Change-Id: Ib63b705c368b2c648e73defa611a68abc2731e84
    Signed-off-by: platania <platania@research.att.com>

--- vnfs/vFW/scripts/run_traffic_fw_demo.sh
+++ vnfs/vFW/scripts/run_traffic_fw_demo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -X PUT -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: 9005870c-900b-2e2e-0902-ef2009bb0ff7" -d '{"pg-streams":{"pg-stream": [{"id":"fw_udp1", "is-enabled":"true"},{"id":"fw_udp2", "is-enabled":"true"},{"id":"fw_udp3", "is-enabled":"true"},{"id":"fw_udp4", "is-enabled":"true"},{"id":"fw_udp5", "is-enabled":"true"}]}}' "http://localhost:8183/restconf/config/sample-plugin:sample-plugin/pg-streams"
+#curl -X PUT -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: 9005870c-900b-2e2e-0902-ef2009bb0ff7" -d '{"pg-streams":{"pg-stream": [{"id":"fw_udp1", "is-enabled":"true"},{"id":"fw_udp2", "is-enabled":"true"},{"id":"fw_udp3", "is-enabled":"true"},{"id":"fw_udp4", "is-enabled":"true"},{"id":"fw_udp5", "is-enabled":"true"}]}}' "http://localhost:8183/restconf/config/sample-plugin:sample-plugin/pg-streams"
 
-sleep 300
+#sleep 300
 
 while [ 1 ] 
 do
