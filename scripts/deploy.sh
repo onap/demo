b8766f7f50782f8db11ffc123d274299fce853a3
platania <platania@research.att.com>
2017-03-06 10:47:18 -0500

    Upload new heat templates
    Change-Id: Ibd8139ea442731b604cf241c4621c1a9bc43961b
    Signed-off-by: platania <platania@research.att.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,12 +6,16 @@ set -x
 VERSION="1.0.0-SNAPSHOT"
 
 PATH_TO_BOOT="/w/workspace/demo-master-merge-java/boot"
-PATH_TO_HEAT="/w/workspace/demo-master-merge-java/heat"
+PATH_TO_HEAT_MASTER="/w/workspace/demo-master-merge-java/heat/OpenECOMP"
+PATH_TO_HEAT_VFW="/w/workspace/demo-master-merge-java/heat/vFW"
+PATH_TO_HEAT_VLB="/w/workspace/demo-master-merge-java/heat/vLB"
 PATH_TO_VFW="/w/workspace/demo-master-merge-java/vnfs/vFW/scripts"
 PATH_TO_VLB="/w/workspace/demo-master-merge-java/vnfs/vLB/scripts"
 
 BOOT_GROUP_ID="org.openecomp.demo/boot"
-HEAT_GROUP_ID="org.openecomp.demo/heat"
+HEAT_MASTER_GROUP_ID="org.openecomp.demo/heat/OpenECOMP"
+HEAT_VFW_GROUP_ID="org.openecomp.demo/heat/vFW"
+HEAT_VLB_GROUP_ID="org.openecomp.demo/heat/vLB"
 VFW_GROUP_ID="org.openecomp.demo/vnfs/vfw"
 VLB_GROUP_ID="org.openecomp.demo/vnfs/vlb"
 
@@ -26,7 +30,13 @@ echo "machine nexus.openecomp.org login ${USER} password ${PASS}" > "${NETRC}"
 cd $PATH_TO_BOOT
 ls | xargs -I{} curl -vk --netrc-file "${NETRC}" --upload-file {} $REPO_URL/$BOOT_GROUP_ID/$VERSION/{}
 
-cd $PATH_TO_HEAT
+cd $PATH_TO_HEAT_MASTER
+ls | xargs -I{} curl -vk --netrc-file "${NETRC}" --upload-file {} $REPO_URL/$HEAT_GROUP_ID/$VERSION/{}
+
+cd $PATH_TO_HEAT_VFW
+ls | xargs -I{} curl -vk --netrc-file "${NETRC}" --upload-file {} $REPO_URL/$HEAT_GROUP_ID/$VERSION/{}
+
+cd $PATH_TO_HEAT_VLB
 ls | xargs -I{} curl -vk --netrc-file "${NETRC}" --upload-file {} $REPO_URL/$HEAT_GROUP_ID/$VERSION/{}
 
 cd $PATH_TO_VFW
