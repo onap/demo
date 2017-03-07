7ffc784e98f5f608000d1deff778e5aa1496a4fc
platania <platania@research.att.com>
2017-03-07 15:29:33 -0500

    Update structure of POM files
    Change-Id: I24ceb375aa3c89b311fb0a0e1b40f8e8ae719343
    Signed-off-by: platania <platania@research.att.com>

--- boot/asdc_vm_init.sh
+++ boot/asdc_vm_init.sh
@@ -5,6 +5,7 @@ NEXUS_PASSWD=$(cat /opt/config/nexus_password.txt)
 NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt)
 NEXUS_DOCKER_PORT=$(echo $NEXUS_DOCKER_REPO | cut -d':' -f2)
 ENV_NAME=$(cat /opt/config/env_name.txt)
+MR_IP_ADDR=$(cat /opt/config/mr_ip_addr.txt)
 RELEASE=latest
 
 cd /opt/sdc
@@ -20,8 +21,8 @@ chmod +x /data/scripts/docker_health.sh
 IP_ADDRESS=$(ifconfig eth0 | grep "inet addr" | tr -s ' ' | cut -d' ' -f3 | cut -d':' -f2)
 cat /data/environments/Template.json | sed "s/yyy/"$IP_ADDRESS"/g" > /data/environments/$ENV_NAME.json
 sed -i "s/xxx/"$ENV_NAME"/g" /data/environments/$ENV_NAME.json
-sed -i "s/\"ueb_url_list\":.*/\"ueb_url_list\": \"10.0.11.1,10.0.11.1\",/g" /data/environments/$ENV_NAME.json
-sed -i "s/\"fqdn\":.*/\"fqdn\": [\"10.0.11.1\", \"10.0.11.1\"]/g" /data/environments/$ENV_NAME.json
+sed -i "s/\"ueb_url_list\":.*/\"ueb_url_list\": \""$MR_IP_ADDR","$MR_IP_ADDR"\",/g" /data/environments/$ENV_NAME.json
+sed -i "s/\"fqdn\":.*/\"fqdn\": [\""$MR_IP_ADDR"\", \""$MR_IP_ADDR"\"]/g" /data/environments/$ENV_NAME.json
 
 docker login -u $NEXUS_USERNAME -p $NEXUS_PASSWD $NEXUS_DOCKER_REPO
 bash /data/scripts/docker_run.sh -e $ENV_NAME -r $RELEASE -p $NEXUS_DOCKER_PORT
