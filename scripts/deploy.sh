87ab6586578f298552bcdddd0b46ad6f255a26a5
Marco Platania <platania@research.att.com>
2017-03-24 15:21:51 -0400

    update deploy script for testing
    Change-Id: I6c534a03566085367c5bab2e65845676e2222ddf
    Signed-off-by: Marco Platania <platania@research.att.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,36 @@
 set -x
 
 ########## Define parameters ##########
-VERSION="1.1.0-SNAPSHOT"
+source ${WORKSPACE}"/version.properties"
+BRANCH=$(echo ${X} | cut -d'-' -f2)
+if [[ $BRANCH == "master" ]]
+then
+	VERSION=$snapshot_version
+else
+	VERSION=$release_version
+fi
 
-PATH_TO_BOOT="/w/workspace/demo-master-merge-java/boot"
-PATH_TO_HEAT="/w/workspace/demo-master-merge-java/heat"
-PATH_TO_HEAT_MASTER="/w/workspace/demo-master-merge-java/heat/OpenECOMP"
-PATH_TO_HEAT_VFW="/w/workspace/demo-master-merge-java/heat/vFW"
-PATH_TO_HEAT_VLB="/w/workspace/demo-master-merge-java/heat/vLB"
-PATH_TO_VFW="/w/workspace/demo-master-merge-java/vnfs/vFW/scripts"
-PATH_TO_VLB="/w/workspace/demo-master-merge-java/vnfs/vLB/scripts"
+echo "This is the workspace: " ${WORKSPACE}
+echo "This is the branch: " $BRANCH
+echo "This is the version number: " $VERSION
+
+PATH_TO_BOOT=${WORKSPACE}"/boot"
+PATH_TO_HEAT=${WORKSPACE}"/heat"
+PATH_TO_HEAT_MASTER=${WORKSPACE}"/heat/OpenECOMP"
+PATH_TO_HEAT_VFW=${WORKSPACE}"/heat/vFW"
+PATH_TO_HEAT_VLB=${WORKSPACE}"/heat/vLB"
+PATH_TO_VFW=${WORKSPACE}"/vnfs/vFW/scripts"
+PATH_TO_VLB=${WORKSPACE}"/vnfs/vLB/scripts"
+
+#VERSION="1.1.0-SNAPSHOT"
+
+#PATH_TO_BOOT="/w/workspace/demo-master-merge-java/boot"
+#PATH_TO_HEAT="/w/workspace/demo-master-merge-java/heat"
+#PATH_TO_HEAT_MASTER="/w/workspace/demo-master-merge-java/heat/OpenECOMP"
+#PATH_TO_HEAT_VFW="/w/workspace/demo-master-merge-java/heat/vFW"
+#PATH_TO_HEAT_VLB="/w/workspace/demo-master-merge-java/heat/vLB"
+#PATH_TO_VFW="/w/workspace/demo-master-merge-java/vnfs/vFW/scripts"
+#PATH_TO_VLB="/w/workspace/demo-master-merge-java/vnfs/vLB/scripts"
 
 BOOT_GROUP_ID="org.openecomp.demo/boot"
 HEAT_GROUP_ID="org.openecomp.demo/heat"
