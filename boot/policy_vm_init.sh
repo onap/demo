73fb16f5b582a81ce92a654567d707e2cd052976
Marco Platania <platania@research.att.com>
2017-05-18 14:59:40 -0400

    Fix path to docker in Policy
    Change-Id: Ib875a439789d6ba6e058d452549eb5f5c85bdb78
    Signed-off-by: Marco Platania <platania@research.att.com>

--- boot/policy_vm_init.sh
+++ boot/policy_vm_init.sh
@@ -23,16 +23,16 @@ echo $IP_ADDRESS > config/pe/ip_addr.txt
 
 docker login -u $NEXUS_USERNAME -p $NEXUS_PASSWD $NEXUS_DOCKER_REPO
 
-docker pull $NEXUS_DOCKER_REPO/openecomp/policy/policy-db:$DOCKER_IMAGE_VERSION
-docker tag $NEXUS_DOCKER_REPO/openecomp/policy/policy-db:$DOCKER_IMAGE_VERSION openecomp/policy/policy-db:latest
+docker pull $NEXUS_DOCKER_REPO/onap/policy/policy-db:$DOCKER_IMAGE_VERSION
+docker tag $NEXUS_DOCKER_REPO/onap/policy/policy-db:$DOCKER_IMAGE_VERSION onap/policy/policy-db:latest
 
-docker pull $NEXUS_DOCKER_REPO/openecomp/policy/policy-pe:$DOCKER_IMAGE_VERSION
-docker tag $NEXUS_DOCKER_REPO/openecomp/policy/policy-pe:$DOCKER_IMAGE_VERSION openecomp/policy/policy-pe:latest
+docker pull $NEXUS_DOCKER_REPO/onap/policy/policy-pe:$DOCKER_IMAGE_VERSION
+docker tag $NEXUS_DOCKER_REPO/onap/policy/policy-pe:$DOCKER_IMAGE_VERSION onap/policy/policy-pe:latest
 
-docker pull $NEXUS_DOCKER_REPO/openecomp/policy/policy-drools:$DOCKER_IMAGE_VERSION
-docker tag $NEXUS_DOCKER_REPO/openecomp/policy/policy-drools:$DOCKER_IMAGE_VERSION openecomp/policy/policy-drools:latest
+docker pull $NEXUS_DOCKER_REPO/onap/policy/policy-drools:$DOCKER_IMAGE_VERSION
+docker tag $NEXUS_DOCKER_REPO/onap/policy/policy-drools:$DOCKER_IMAGE_VERSION onap/policy/policy-drools:latest
 
-docker pull $NEXUS_DOCKER_REPO/openecomp/policy/policy-nexus:$DOCKER_IMAGE_VERSION
-docker tag $NEXUS_DOCKER_REPO/openecomp/policy/policy-nexus:$DOCKER_IMAGE_VERSION openecomp/policy/policy-nexus:latest
+docker pull $NEXUS_DOCKER_REPO/onap/policy/policy-nexus:$DOCKER_IMAGE_VERSION
+docker tag $NEXUS_DOCKER_REPO/onap/policy/policy-nexus:$DOCKER_IMAGE_VERSION onap/policy/policy-nexus:latest
 
 /opt/docker/docker-compose up -d
