0e385f5330006e18986edeec1c5b73021da93680
Marco Platania <platania@research.att.com>
2017-05-23 15:21:37 -0400

    fix portal/dns gre issues
    Change-Id: I652e5d8d4389b95feec8ec8b3b953748d3644d41
    Signed-off-by: Marco Platania <platania@research.att.com>

--- boot/portal_vm_init.sh
+++ boot/portal_vm_init.sh
@@ -35,12 +35,17 @@ sleep 180
 
 if [ ! -e /opt/config/boot.txt ]
 then
-  if [ -e /opt/config/public_ip.txt ]
-  then
-    IP_ADDRESS=$(cat /opt/config/public_ip.txt)
-  else
-    IP_ADDRESS=$(ifconfig eth0 | grep "inet addr" | tr -s ' ' | cut -d' ' -f3 | cut -d':' -f2)
-  fi
-  mysql -u root -p'Aa123456' -h $IP_ADDRESS < /opt/portal/deliveries/Apps_Users_OnBoarding_Script.sql
-  echo "yes" > /opt/config/boot.txt
+	if [ -e /opt/config/public_ip.txt ]
+	then
+		IP_ADDRESS=$(cat /opt/config/public_ip.txt)
+	else
+		IP_ADDRESS=$(ifconfig eth0 | grep "inet addr" | tr -s ' ' | cut -d' ' -f3 | cut -d':' -f2)
+	fi
+	# Wait until MySQL is running...
+	while [[ $(netstat -vulntp |grep -i mysql | awk '{print $4}') != ":::3306" ]]
+	do
+		sleep 1
+	done
+	mysql -u root -p'Aa123456' -h $IP_ADDRESS < /opt/portal/deliveries/Apps_Users_OnBoarding_Script.sql
+	echo "yes" > /opt/config/boot.txt
 fi
