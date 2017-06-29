408e5ba68e74c9c8fe642f6b06a5581a6761a344
Nayan Deshmukh <n.deshmukh@samsung.com>
2017-06-29 18:52:39 +0900

    avoid hardcoded values
    in integration_robot_properties.py
    
    Change-Id: I8bd992d7e10dc87077d80506ef8710cb171c6329
    Signed-off-by: Nayan Deshmukh <n.deshmukh@samsung.com>

--- boot/robot_vm_init.sh
+++ boot/robot_vm_init.sh
@@ -5,6 +5,7 @@ NEXUS_PASSWD=$(cat /opt/config/nexus_password.txt)
 NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt)
 NEXUS_REPO=$(cat /opt/config/nexus_repo.txt)
 DOCKER_IMAGE_VERSION=$(cat /opt/config/docker_version.txt)
+CLOUD_ENV=$(cat /opt/config/cloud_env.txt)
 
 #
 # Deploy latest robot configuration 
@@ -15,6 +16,21 @@ cp integration_* /opt/eteshare/config
 cp vm_config2robot.sh /opt/eteshare/config
 cp ete.sh /opt
 cp demo.sh /opt
+
+if [[ $CLOUD_ENV != "rackspace" ]]
+then
+	sed -i "s/10.0.1.1/"$(cat /opt/config/aai1_ip_addr.txt)"/g" /opt/eteshare/config/integration_robot_properties.py
+	sed -i "s/10.0.2.1/"$(cat /opt/config/appc_ip_addr.txt)"/g" /opt/eteshare/config/integration_robot_properties.py
+	sed -i "s/10.0.4.1/"$(cat /opt/config/dcae_ip_addr.txt)"/g" /opt/eteshare/config/integration_robot_properties.py
+	sed -i "s/10.0.5.1/"$(cat /opt/config/mso_ip_addr.txt)"/g" /opt/eteshare/config/integration_robot_properties.py
+	sed -i "s/10.0.11.1/"$(cat /opt/config/mr_ip_addr.txt)"/g" /opt/eteshare/config/integration_robot_properties.py
+	sed -i "s/10.0.6.1/"$(cat /opt/config/policy_ip_addr.txt)"/g" /opt/eteshare/config/integration_robot_properties.py
+	sed -i "s/10.0.9.1/"$(cat /opt/config/portal_ip_addr.txt)"/g" /opt/eteshare/config/integration_robot_properties.py
+	sed -i "s/10.0.3.1/"$(cat /opt/config/sdc_ip_addr.txt)"/g" /opt/eteshare/config/integration_robot_properties.py
+	sed -i "s/10.0.7.1/"$(cat /opt/config/sdnc_ip_addr.txt)"/g" /opt/eteshare/config/integration_robot_properties.py
+	sed -i "s/10.0.8.1/"$(cat /opt/config/vid_ip_addr.txt)"/g" /opt/eteshare/config/integration_robot_properties.py
+	sed -i "s/https:\/\/identity.api.rackspacecloud.com/"$(cat /opt/config/keystone.txt)"/g" /opt/eteshare/config/integration_robot_properties.py
+fi
 
 chmod +x /opt/ete.sh
 chmod +x /opt/demo.sh
