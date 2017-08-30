7f79a4cea0c2c5e7d00c2ec27b85d22f9b77b956
Marco Platania <platania@research.att.com>
2017-08-30 10:40:14 -0400

    Fix API_KEY file name in CLAMP scripts
    Change-Id: Ibb7f49f0ab85bd5d93370e62dc44b5dc1ea69513
    Issue-ID: INT-115
    Signed-off-by: Marco Platania <platania@research.att.com>

--- boot/clamp_vm_init.sh
+++ boot/clamp_vm_init.sh
@@ -5,7 +5,7 @@ NEXUS_PASSWD=$(cat /opt/config/nexus_password.txt)
 NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt)
 DMAAP_TOPIC=$(cat /opt/config/dmaap_topic.txt)
 OPENSTACK_USERNAME=$(cat /opt/config/openstack_username.txt)
-OPENSTACK_APIKEY=$(cat /opt/config/api_key.txt)
+OPENSTACK_API_KEY=$(cat /opt/config/openstack_api_key.txt)
 
 # Fetch the latest code/scripts
 cd /opt/clamp
