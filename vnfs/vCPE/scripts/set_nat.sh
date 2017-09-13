2913e933164ea8874fe048ca3bc42d6069f14dd3
Johnson Li <johnson.li@intel.com>
2017-09-13 15:14:36 +0800

    Add patch for BRG and scripts to set the NAT rule
    Add Framework codes for the vCPE VNFs
    
    Issue-ID: INT-80
    Change-Id: I312aef9631636f0e2273f482f5db44ca93a55983
    Signed-off-by: Johnson Li <johnson.li@intel.com>
    Signed-off-by: root <rujun.li@intel.com>

---
+++ vnfs/vCPE/scripts/set_nat.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+while :
+do
+	if [[ ! $(ps -aux | grep [[:alnum:]]*/vpp/startup.conf | wc -l) = 2 ]]; then
+		#echo "vpp not running"
+		continue
+	fi
+	flag=0
+	while read -r line
+	do
+		if [ flag = 0 ]; then
+			re=${line#*/[0-9]/[0-9]}
+			if [ "$line" != "$re" ]; then
+				flag=1
+			else
+				flag=0
+				continue
+			fi
+		else
+			ip=${line%/*}
+			if [[ $ip = *\.*\.*\.* ]]; then
+				#echo "ip address is $ip"
+				if [ ! -f /etc/vpp/ip.txt ]; then
+					echo "file /etc/vpp/ip.txt doesn't exists"
+					continue
+				fi
+				while read -r tap_ip
+				do
+					if [[ $tap_ip = hcip* ]]; then
+						tap_ip=${tap_ip#*" "}
+						echo "hc tap ip address is $tap_ip"
+						vppctl snat add static mapping local $tap_ip external $ip
+						exit 0
+					fi
+				done < /etc/vpp/ip.txt
+			else
+				if [[ ! $ip = */[0-9] ]]; then
+					flag=0
+					echo "not correct"
+				fi
+			fi
+		fi
+	done < <(vppctl show int addr)
+done
