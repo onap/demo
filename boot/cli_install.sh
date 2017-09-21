60d64559eb69636e3f5f19c5fe8065deeb1adc6f
Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>
2017-09-21 18:30:05 +0530

    Add install script for CLI
    Installer script to download the latest CLI
    from the nexus and install it locally with
    all required software.
    
    CLI-47
    
    Change-Id: I4c77991012bfb79f3e4157a53e0e46df0b899a4e
    Signed-off-by: Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>

---
+++ boot/cli_install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#*******************************************************************************
+# Copyright 2017 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#*******************************************************************************
+
+CLI_LATEST_BINARY="https://nexus.onap.org/service/local/artifact/maven/redirect?r=snapshots&g=org.onap.cli&a=cli-zip&e=zip&v=LATEST"
+CLI_INSTALL_DIR=/opt/onap/cli
+CLI_ZIP=cli.zip
+CLI_BIN=/usr/bin/onap
+export ONAP_CLI_HOME=$CLI_INSTALL_DIR
+
+#create install dir
+if [ -d $CLI_INSTALL_DIR ]
+then
+    mv $CLI_INSTALL_DIR $CLI_INSTALL_DIR/../cli_`date +"%m-%d-%y-%H-%M-%S"`
+    rm $CLI_BIN
+fi
+
+mkdir -p $CLI_INSTALL_DIR
+cd $CLI_INSTALL_DIR
+
+#Download and unzip CLI
+apt-get install -y wget unzip openjdk-8-jre
+wget -O $CLI_ZIP $CLI_LATEST_BINARY
+
+unzip $CLI_ZIP
+if [ ! -d ./data ]; then mkdir ./data; fi
+if [ ! -d ./onap-cli-schema ]; then mkdir ./onap-cli-schema; fi
+chmod +x ./bin/onap.sh
+
+#Make onap available in path
+ln ./bin/onap.sh $CLI_BIN
+
+#Print the version
+onap -v
+
+cd -
