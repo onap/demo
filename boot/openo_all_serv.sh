55543b2563fc6e4ff2b42f881538388ade96c71f
Marco Platania <platania@research.att.com>
2017-09-21 15:04:56 -0400

    Parametrize MSB IP in VF-C installation
    Change-Id: I3a1dbaec578b5eace44d7bb746a0df398a1047aa
    Issue-ID: INT-213
    Signed-off-by: Marco Platania <platania@research.att.com>

--- boot/openo_all_serv.sh
+++ boot/openo_all_serv.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-bash /opt/msb_vm_init.sh
-sleep 2
-bash /opt/vnfsdk_vm_init.sh
-sleep 2
-bash /opt/mvim_vm_init.sh
-sleep 2
-bash /opt/vfc_vm_init.sh
-sleep 2
-bash /opt/uui_vm_init.sh
+bash /opt/msb_vm_init.sh &>/dev/null &disown
+bash /opt/vnfsdk_vm_init.sh &>/dev/null &disown
+bash /opt/mvim_vm_init.sh &>/dev/null &disown
+bash /opt/vfc_vm_init.sh &>/dev/null &disown
+bash /opt/uui_vm_init.sh &>/dev/null &disown
--- boot/vfc_vm_init.sh
+++ boot/vfc_vm_init.sh
@@ -6,6 +6,8 @@ NEXUS_PASSWD=$(cat /opt/config/nexus_password.txt)
 NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt)
 DOCKER_IMAGE_VERSION=$(cat /opt/config/vfc_docker.txt)
 
+source /opt/config/onap_ips.txt
+
 # Refresh images
 docker login -u $NEXUS_USERNAME -p $NEXUS_PASSWD $NEXUS_DOCKER_REPO
 docker pull $NEXUS_DOCKER_REPO/onap/vfc/catalog:$DOCKER_IMAGE_VERSION
@@ -21,16 +23,29 @@ docker pull $NEXUS_DOCKER_REPO/onap/vfc/vnfres:$DOCKER_IMAGE_VERSION
 docker pull $NEXUS_DOCKER_REPO/onap/vfc/ztesdncdriver:$DOCKER_IMAGE_VERSION
 docker pull $NEXUS_DOCKER_REPO/onap/vfc/ztevmanagerdriver:$DOCKER_IMAGE_VERSION
 
+docker rm -f vfc_catalog
+docker rm -f vfc_emsdriver
+docker rm -f vfc_gvnfmdriver
+docker rm -f vfc_jujudriver
+docker rm -f vfc_svnfm_huawei
+docker rm -f vfc_nslcm
+docker rm -f vfc_resmanagement
+docker rm -f vfc_vnflcm
+docker rm -f vfc_vnfmgr
+docker rm -f vfc_vnfres
+docker rm -f vfc_ztesdncdriver
+docker rm -f vfc_ztevmanagerdriver
+
 # Insert docker run instructions here
-docker run -i -t -d --name vfc_catalog -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/catalog:$DOCKER_IMAGE_VERSION
-docker run -i -t -d --name vfc_emsdriver -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/emsdriver:$DOCKER_IMAGE_VERSION
-docker run -i -t -d --name vfc_gvnfmdriver -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/gvnfmdriver:$DOCKER_IMAGE_VERSION
-docker run -i -t -d --name vfc_jujudriver -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/jujudriver:$DOCKER_IMAGE_VERSION
-docker run -i -t -d --name vfc_svnfm_huawei -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/nfvo/svnfm/huawei:$DOCKER_IMAGE_VERSION
-docker run -i -t -d --name vfc_nslcm -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/nslcm:$DOCKER_IMAGE_VERSION
-docker run -i -t -d --name vfc_resmanagement -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/resmanagement:$DOCKER_IMAGE_VERSION
-docker run -i -t -d --name vfc_vnflcm -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/vnflcm:$DOCKER_IMAGE_VERSION
-docker run -i -t -d --name vfc_vnfmgr -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/vnfmgr:$DOCKER_IMAGE_VERSION
-docker run -i -t -d --name vfc_vnfres -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/vnfres:$DOCKER_IMAGE_VERSION
-docker run -i -t -d --name vfc_ztesdncdriver -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/ztesdncdriver:$DOCKER_IMAGE_VERSION
-docker run -i -t -d --name vfc_ztevmanagerdriver -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/ztevmanagerdriver:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_catalog -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/catalog:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_emsdriver -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/emsdriver:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_gvnfmdriver -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/gvnfmdriver:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_jujudriver -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/jujudriver:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_svnfm_huawei -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/nfvo/svnfm/huawei:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_nslcm -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/nslcm:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_resmanagement -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/resmanagement:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_vnflcm -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/vnflcm:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_vnfmgr -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/vnfmgr:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_vnfres -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/vnfres:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_ztesdncdriver -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/ztesdncdriver:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_ztevmanagerdriver -e MSB_ADDR=$OPENO_IP:80 $NEXUS_DOCKER_REPO/onap/vfc/ztevmanagerdriver:$DOCKER_IMAGE_VERSION
