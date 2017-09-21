de55cfb8a4716b280b5af744aabff6309d86ffcd
ying.yunlong <ying.yunlong@zte.com.cn>
2017-09-21 11:10:59 +0800

    Add vfc docker run instructions in demo
    Change-Id: I3915b0654c06c3e923ea71f5bd7605ca0160ad9e
    Issue-ID: VFC-404
    Signed-off-by: ying.yunlong <ying.yunlong@zte.com.cn>

--- boot/vfc_vm_init.sh
+++ boot/vfc_vm_init.sh
@@ -22,3 +22,15 @@ docker pull $NEXUS_DOCKER_REPO/onap/vfc/ztesdncdriver:$DOCKER_IMAGE_VERSION
 docker pull $NEXUS_DOCKER_REPO/onap/vfc/ztevmanagerdriver:$DOCKER_IMAGE_VERSION
 
 # Insert docker run instructions here
+docker run -i -t -d --name vfc_catalog -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/catalog:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_emsdriver -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/emsdriver:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_gvnfmdriver -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/gvnfmdriver:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_jujudriver -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/jujudriver:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_svnfm_huawei -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/nfvo/svnfm/huawei:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_nslcm -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/nslcm:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_resmanagement -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/resmanagement:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_vnflcm -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/vnflcm:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_vnfmgr -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/vnfmgr:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_vnfres -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/vnfres:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_ztesdncdriver -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/ztesdncdriver:$DOCKER_IMAGE_VERSION
+docker run -i -t -d --name vfc_ztevmanagerdriver -e MSB_ADDR=127.0.0.1:80 $NEXUS_DOCKER_REPO/onap/vfc/ztevmanagerdriver:$DOCKER_IMAGE_VERSION
