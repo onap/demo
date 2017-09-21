bc20b841d963c06d7c0c2f36b28a109e9a1e5ca5
Ethan Lynn <ethanlynnl@vmware.com>
2017-09-21 09:31:07 +0800

    Fix MultiCloud Environment variable
    Correct name is MSB_ADDR.
    
    Change-Id: Ib92b4222df824b11baaf96b62e4e6b0dc369ff7b
    issue-id: MULTICLOUD-97
    Signed-off-by: Ethan Lynn <ethanlynnl@vmware.com>

--- boot/mvim_vm_init.sh
+++ boot/mvim_vm_init.sh
@@ -16,5 +16,5 @@ docker pull $NEXUS_DOCKER_REPO/onap/multicloud/vio:$DOCKER_IMAGE_VERSION
 docker rm -f multicloud-broker
 docker rm -f multicloud-vio
 
-docker run -e MS_ADDR=$OPENO_IP -e AAI_ADDR=$AAI_IP1 -p 9001:9001 --name multicloud-broker $NEXUS_DOCKER_REPO/onap/multicloud/framework:$DOCKER_IMAGE_VERSION
-docker run -e MS_ADDR=$OPENO_IP -e AAI_ADDR=$AAI_IP1 -p 9004:9004 --name multicloud-vio $NEXUS_DOCKER_REPO/onap/multicloud/vio:$DOCKER_IMAGE_VERSION
+docker run -e MSB_ADDR=$OPENO_IP -e AAI_ADDR=$AAI_IP1 -p 9001:9001 --name multicloud-broker $NEXUS_DOCKER_REPO/onap/multicloud/framework:$DOCKER_IMAGE_VERSION
+docker run -e MSB_ADDR=$OPENO_IP -e AAI_ADDR=$AAI_IP1 -p 9004:9004 --name multicloud-vio $NEXUS_DOCKER_REPO/onap/multicloud/vio:$DOCKER_IMAGE_VERSION
