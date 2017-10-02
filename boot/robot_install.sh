c33040b161f78f9d750c28cd097c6de5e900c5bc
Marco Platania <platania@research.att.com>
2017-10-01 22:29:14 -0400

    Fix bugs in the installation process
    - Add short-term fix to robot script after MSO/SO renaming
    - Change docker version number in SDNC
    
    Change-Id: I625e09457b843c875ea19c69ed6078f88a978f17
    Issue-ID: INT-213
    Signed-off-by: Marco Platania <platania@research.att.com>

--- boot/robot_install.sh
+++ boot/robot_install.sh
@@ -9,6 +9,9 @@ GERRIT_BRANCH=$(cat /opt/config/gerrit_branch.txt)
 MTU=$(/sbin/ifconfig | grep MTU | sed 's/.*MTU://' | sed 's/ .*//' | sort -n | head -1)
 CODE_REPO=$(cat /opt/config/remote_repo.txt)
 
+# Short-term fix to get around MSO to SO name change
+cp /opt/config/so_ip_addr.txt /opt/config/mso_ip_addr.txt
+
 # Add host name to /etc/host to avoid warnings in openstack images
 if [[ $CLOUD_ENV != "rackspace" ]]
 then
