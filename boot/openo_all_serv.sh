a81fdca54b756d50d9aaaa9c6da12fc239984e77
lizi <li.zi30@zte.com.cn>
2017-10-09 19:41:24 +0800

    Add docker run instruction in demo for esr.
    Change-Id: Ic9b1322835e8277b26ffb95a10ccce3829a0d1e3
    Issue-ID: AAI-421
    Signed-off-by: lizi <li.zi30@zte.com.cn>

--- boot/openo_all_serv.sh
+++ boot/openo_all_serv.sh
@@ -5,3 +5,4 @@ bash /opt/vnfsdk_vm_init.sh &>/dev/null &disown
 bash /opt/mvim_vm_init.sh &>/dev/null &disown
 bash /opt/vfc_vm_init.sh &>/dev/null &disown
 bash /opt/uui_vm_init.sh &>/dev/null &disown
+bash /opt/esr_vm_init.sh &>/dev/null &disown
