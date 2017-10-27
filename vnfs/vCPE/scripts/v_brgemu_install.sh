5fec1ec4a7a519f52a53987e894b2cc68f184c1b
Itohan <itohan.ukponmwan@intel.com>
2017-10-26 22:52:47 -0700

    Fixed vbrg Install, env and yaml scipts
    Fixed bugs in vCPE vbrg emulator install script
    Modified Script to include different build modes
    Fixed here documents bind_nic.sh and set_nat.sh
    Commented out the 192.168.1.0/24 network
    configurations in the yaml and env files
    
    Change-Id: I2f6dee1843e6e289fe199844ed80d010257bce63
    Issue-ID: INT-63
    Signed-off-by: Itohan <itohan.ukponmwan@intel.com>

--- vnfs/vCPE/scripts/v_brgemu_install.sh
+++ vnfs/vCPE/scripts/v_brgemu_install.sh
@@ -33,68 +33,55 @@ cdr2mask () {
 # OpenStack network configuration
 if [[ $BUILD_STATE != "build" ]]
 then
-	if [[ $CLOUD_ENV == "openstack" ]]
-	then
+    if [[ $CLOUD_ENV == "openstack" ]]
+    then
         echo 127.0.0.1 $(hostname) >> /etc/hosts
 
         # Allow remote login as root
         mv /root/.ssh/authorized_keys /root/.ssh/authorized_keys.bk
         cp /home/ubuntu/.ssh/authorized_keys /root/.ssh
-
-        #MTU=$(/sbin/ifconfig | grep MTU | sed 's/.*MTU://' | sed 's/ .*//' | sort -n | head -1)
-
-        #IP=$(cat /opt/config/brgemu_net_ipaddr.txt)
-        #BITS=$(cat /opt/config/brgemu_net_cidr.txt | cut -d"/" -f2)
-        #NETMASK=$(cdr2mask $BITS)
-        #echo "auto eth2" >> /etc/network/interfaces
-        #echo "iface eth2 inet static" >> /etc/network/interfaces
-        #echo "    address $IP" >> /etc/network/interfaces
-        #echo "    netmask $NETMASK" >> /etc/network/interfaces
-        #echo "    mtu $MTU" >> /etc/network/interfaces
-
-        #ifup eth2
-	fi
+    fi
 fi  # endif BUILD_STATE != "build"
 
 if [[ $BUILD_STATE != "done" ]]
 then
-	# Download required dependencies
-	echo "deb http://ppa.launchpad.net/openjdk-r/ppa/ubuntu $(lsb_release -c -s) main" >>  /etc/apt/sources.list.d/java.list
-	echo "deb-src http://ppa.launchpad.net/openjdk-r/ppa/ubuntu $(lsb_release -c -s) main" >>  /etc/apt/sources.list.d/java.list
-	apt-get update
-	apt-get install --allow-unauthenticated -y wget openjdk-8-jdk apt-transport-https ca-certificates g++ libcurl4-gnutls-dev 
-	sleep 1
+    # Download required dependencies
+    echo "deb http://ppa.launchpad.net/openjdk-r/ppa/ubuntu $(lsb_release -c -s) main" >>  /etc/apt/sources.list.d/java.list
+    echo "deb-src http://ppa.launchpad.net/openjdk-r/ppa/ubuntu $(lsb_release -c -s) main" >>  /etc/apt/sources.list.d/java.list
+    apt-get update
+    apt-get install --allow-unauthenticated -y wget openjdk-8-jdk apt-transport-https ca-certificates g++ libcurl4-gnutls-dev
+    sleep 1
 
-	# Install the tools required for download codes
-	apt-get install -y expect git patch make linux-image-extra-`uname -r`
-	#Download and build the VPP codes
-	cd /opt
-	git clone ${VPP_SOURCE_REPO_URL} -b ${VPP_SOURCE_REPO_BRANCH} vpp
-	wget -O VPP-Add-Option82-Nat-Filter-For-vBRG.patch ${VPP_PATCH_URL}
+    # Install the tools required for download codes
+    apt-get install -y expect git patch make linux-image-extra-`uname -r`
+    #Download and build the VPP codes
+    cd /opt
+    git clone ${VPP_SOURCE_REPO_URL} -b ${VPP_SOURCE_REPO_BRANCH} vpp
+    wget -O VPP-Add-Option82-Nat-Filter-For-vBRG.patch ${VPP_PATCH_URL}
 
-	cd vpp
-	patch -p1 < ../VPP-Add-Option82-Nat-Filter-For-vBRG.patch
-	expect -c "
-			set timeout 60;
-			spawn make install-dep;
-			expect {
-					\"Do you want to continue?*\" {send \"Y\r\"; interact}
-			}
-	"
+    cd vpp
+    patch -p1 < ../VPP-Add-Option82-Nat-Filter-For-vBRG.patch
+    expect -c "
+            set timeout 60;
+            spawn make install-dep;
+            expect {
+                    \"Do you want to continue?*\" {send \"Y\r\"; interact}
+            }
+    "
 
-	cd build-root
-	./bootstrap.sh
-	make V=0 PLATFORM=vpp TAG=vpp install-deb
+    cd build-root
+    ./bootstrap.sh
+    make V=0 PLATFORM=vpp TAG=vpp install-deb
 
-	# Install the VPP package
-	dpkg -i *.deb
-	systemctl stop vpp
+    # Install the VPP package
+    dpkg -i *.deb
+    systemctl stop vpp
 fi  # endif BUILD_STATE != "done"
 
 if [[ $BUILD_STATE != "build" ]]
 then
-	# Auto-start configuration for the VPP
-	cat > /etc/vpp/startup.conf << EOF
+    # Auto-start configuration for the VPP
+    cat > /etc/vpp/startup.conf << EOF
 
 unix {
   nodaemon
@@ -122,7 +109,7 @@ cpu {
         # main-core 1
 
         ## Set logical CPU core(s) where worker threads are running
-		# corelist-workers 2-3,18-19
+        # corelist-workers 2-3,18-19
 
         ## Automatic pinning of thread(s) to CPU core(s)
 
@@ -172,7 +159,7 @@ cpu {
 
         ## Whitelist specific interface by specifying PCI address
         # dev 0000:02:00.0
-		## Whitelist specific interface by specifying PCI address and in
+        ## Whitelist specific interface by specifying PCI address and in
         ## addition specify custom parameters for this interface
         # dev 0000:02:00.1 {
         #       num-rx-queues 2
@@ -198,7 +185,7 @@ cpu {
 
 EOF
 
-	#Get list of network device PCI bus addresses
+    #Get list of network device PCI bus addresses
     get_nic_pci_list() {
         while read -r line ; do
             if [ "$line" != "${line#*network device}" ]; then
@@ -213,7 +200,7 @@ EOF
 
     BRG_BNG_NIC=GigabitEthernet`echo ${NICS} | cut -d " " -f 2`  # second interface in list
  
-	cat > /etc/vpp/setup.gate << EOF
+    cat > /etc/vpp/setup.gate << EOF
 set int state ${BRG_BNG_NIC} up
 set dhcp client intfc ${BRG_BNG_NIC} hostname brg-emulator
 
@@ -224,11 +211,11 @@ set interface l2 bridge tap-0 10 0
 set bridge-domain arp term 10
 EOF
 
-	cat >> /opt/config/ip.txt << EOF
+    cat >> /opt/config/ip.txt << EOF
 hcip: 192.168.4.20
 EOF
 
-	cat > /opt/bind_nic.sh << 'EOF'
+    cat > /opt/bind_nic.sh << 'EOF'
 #!/bin/bash
 while :
 do
@@ -255,88 +242,88 @@ vppctl snat add interface address ${BRG_BNG_NIC}
 
 while read -r hw
 do
-        if [[ "$hw" = tap-1* ]]; then
-                read -r hw
-                hw_addr=${hw##* }
-                break
-        fi
+    if [[ "$hw" = tap-1* ]]; 
+    then
+        read -r hw
+        hw_addr=${hw##* }
+        break
+    fi
 done < <(vppctl show hardware)
 arp -s $sdnc_ip $hw_addr
 EOF
-	chmod +x /opt/bind_nic.sh
+    chmod +x /opt/bind_nic.sh
 
-	#set nat rule
-	cat > /opt/set_nat.sh << 'EOF'
+    #set nat rule
+    cat > /opt/set_nat.sh << 'EOF'
 #! /bin/bash
 
 while :
 do
-        if [[ ! $(ps -aux | grep [[:alnum:]]*/vpp/startup.conf | wc -l) = 2 ]]; then
-                #echo "vpp not running"
+    if [[ ! $(ps -aux | grep [[:alnum:]]*/vpp/startup.conf | wc -l) = 2 ]]; 
+    then
+        #echo "vpp not running"
+        continue
+    fi
+
+    flag=0
+    while read -r line
+    do
+        if [ flag = 0 ];
+        then
+            re=${line#*/[0-9]/[0-9]}
+            if [ "$line" != "$re" ];
+            then
+                flag=1
+            else
+                flag=0
                 continue
-        fi
-
-        flag=0
-        while read -r line
-        do
-                if [ flag = 0 ]; 
-				then
-				        re=${line#*/[0-9]/[0-9]}
-                        if [ "$line" != "$re" ]; 
-						then
-                                flag=1
-                        else
-                                flag=0
-                                continue
-                        fi
-                else
-                        ip=${line%/*}
-                        if [[ $ip = *\.*\.*\.* ]]; 
-						then
-                                #echo "ip address is $ip"
-                                if [ ! -f /opt/config/ip.txt ];
-								then
-                                        echo "file /opt/config/ip.txt doesn't exists"
-                                        continue
-                                fi
-                                while read -r tap_ip
-                                do
-                                        if [[ $tap_ip = hcip* ]]; 
-										then
-                                                tap_ip=${tap_ip#*" "}
-                                                echo "hc tap ip address is $tap_ip"
-												vppctl snat add static mapping tcp local $tap_ip 8183 external $ip 8183
-
-                                                exit 0
-                                        fi
-                                done < /opt/config/ip.txt
-                        else
-                                if [[ ! $ip = */[0-9] ]]; 
-								then
-                                        flag=0
-                                        #echo "not correct"
-                                fi
-                        fi
+            fi
+        else
+            ip=${line%/*}
+            if [[ $ip = *\.*\.*\.* ]];
+            then
+                #echo "ip address is $ip"
+                if [ ! -f /opt/config/ip.txt ];
+                then
+                    echo "file /opt/config/ip.txt doesn't exists"
+                    continue
                 fi
-        done < <(vppctl show int addr)
-        sleep 1
+                while read -r tap_ip
+                do
+                    if [[ $tap_ip = hcip* ]];
+                    then
+                        tap_ip=${tap_ip#*" "}
+                        echo "hc tap ip address is $tap_ip"
+                        vppctl snat add static mapping tcp local $tap_ip 8183 external $ip 8183
+                        exit 0
+                    fi
+                done < /opt/config/ip.txt
+            else
+                if [[ ! $ip = */[0-9] ]];
+                then
+                    flag=0
+                    #echo "not correct"
+                fi
+            fi
+        fi
+    done < <(vppctl show int addr)
+    sleep 1
 done
 EOF
-	chmod +x /opt/set_nat.sh
+    chmod +x /opt/set_nat.sh
 fi  # endif BUILD_STATE != "build"
 
 if [[ $BUILD_STATE != "done" ]]
 then
 
-	# Download and install HC2VPP from source
-	cd /opt
-	git clone ${HC2VPP_SOURCE_REPO_URL} -b ${HC2VPP_SOURCE_REPO_BRANCH} hc2vpp
+    # Download and install HC2VPP from source
+    cd /opt
+    git clone ${HC2VPP_SOURCE_REPO_URL} -b ${HC2VPP_SOURCE_REPO_BRANCH} hc2vpp
 
-	apt-get -f -y install
-	apt-get install -y maven
-	mkdir -p /root/.m2
-	
-	cat > ~/.m2/settings.xml << EOF
+    apt-get -f -y install
+    apt-get install -y maven
+    mkdir -p /root/.m2
+    cat > ~/.m2/settings.xml << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!-- vi: set et smarttab sw=2 tabstop=2: -->
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
@@ -444,28 +431,26 @@ then
 </settings>
 EOF
 
-	cd hc2vpp
-	mvn clean install
-	l_version=$(cat pom.xml | grep "<version>" | head -1)
-	l_version=$(echo "${l_version%<*}")
-	l_version=$(echo "${l_version#*>}")
-	mv vpp-integration/minimal-distribution/target/vpp-integration-distribution-${l_version}-hc/vpp-integration-distribution-${l_version} /opt/honeycomb
-	sed -i 's/127.0.0.1/0.0.0.0/g' /opt/honeycomb/config/honeycomb.json
-	
-	# Disable automatic upgrades
+    cd hc2vpp
+    mvn clean install
+    l_version=$(cat pom.xml | grep "<version>" | head -1)
+    l_version=$(echo "${l_version%<*}")
+    l_version=$(echo "${l_version#*>}")
+    mv vpp-integration/minimal-distribution/target/vpp-integration-distribution-${l_version}-hc/vpp-integration-distribution-${l_version} /opt/honeycomb
+    sed -i 's/127.0.0.1/0.0.0.0/g' /opt/honeycomb/config/honeycomb.json
+
+    # Disable automatic upgrades
     if [[ $CLOUD_ENV != "rackspace" ]]
     then
         echo "APT::Periodic::Unattended-Upgrade \"0\";" >> /etc/apt/apt.conf.d/10periodic
         sed -i 's/\(APT::Periodic::Unattended-Upgrade\) "1"/\1 "0"/' /etc/apt/apt.conf.d/20auto-upgrades
-    fi
-	
-	
+    fi  
 fi  # endif BUILD_STATE != "done"
 
 if [[ $BUILD_STATE != "build" ]]
 then
-	# Create systemctl service for Honeycomb
-	cat > /etc/systemd/system/honeycomb.service << EOF
+    # Create systemctl service for Honeycomb
+    cat > /etc/systemd/system/honeycomb.service << EOF
 [Unit]
 Description=Honeycomb Agent for the VPP control plane
 Documentation=https://wiki.fd.io/view/Honeycomb
@@ -480,30 +465,28 @@ RestartSec=10
 [Install]
 WantedBy=multi-user.target
 EOF
-	systemctl enable /etc/systemd/system/honeycomb.service
+    systemctl enable /etc/systemd/system/honeycomb.service
 
-	# Download DHCP config files
-	cd /opt
-	wget $REPO_URL_BLOB/org.onap.demo/vnfs/vcpe/$INSTALL_SCRIPT_VERSION/v_brgemu_init.sh
-	wget $REPO_URL_BLOB/org.onap.demo/vnfs/vcpe/$INSTALL_SCRIPT_VERSION/v_brgemu.sh
-	sed -i '/# Provides:/c\# Provides:     vbrg ' /opt/v_brgemu.sh
-	chmod +x v_brgemu_init.sh
-	chmod +x v_brgemu.sh
-	mv v_brgemu.sh /etc/init.d
-	update-rc.d v_brgemu.sh defaults
+    # Download DHCP config files
+    cd /opt
+    wget $REPO_URL_BLOB/org.onap.demo/vnfs/vcpe/$INSTALL_SCRIPT_VERSION/v_brgemu_init.sh
+    wget $REPO_URL_BLOB/org.onap.demo/vnfs/vcpe/$INSTALL_SCRIPT_VERSION/v_brgemu.sh
+    sed -i '/# Provides:/c\# Provides:     vbrg ' /opt/v_brgemu.sh
+    chmod +x v_brgemu_init.sh
+    chmod +x v_brgemu.sh
+    mv v_brgemu.sh /etc/init.d
+    update-rc.d v_brgemu.sh defaults
 
-	# Rename network interface in openstack Ubuntu 16.04 images. Then, reboot the VM to pick up changes
-	if [[ $CLOUD_ENV != "rackspace" ]]
-	then
+    # Rename network interface in openstack Ubuntu 16.04 images. Then, reboot the VM to pick up changes
+    if [[ $CLOUD_ENV != "rackspace" ]]
+    then
         sed -i "s/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"net.ifnames=0 biosdevname=0\"/g" /etc/default/grub
         grub-mkconfig -o /boot/grub/grub.cfg
         sed -i "s/ens[0-9]*/eth0/g" /etc/network/interfaces.d/*.cfg
         sed -i "s/ens[0-9]*/eth0/g" /etc/udev/rules.d/70-persistent-net.rules
         echo 'network: {config: disabled}' >> /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
-      
         reboot
-	fi
+    fi
 
-	./v_brgemu_init.sh
+    ./v_brgemu_init.sh
 fi  # endif BUILD_STATE != "build"
-                                  
