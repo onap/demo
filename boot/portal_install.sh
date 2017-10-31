42b74bb5efde813413ab00216324072cbfcb2bb9
Christopher Lott (cl778h) <clott@research.att.com>
2017-10-31 10:14:44 -0400

    Upgrade docker-compose from 1.9.0 to 1.16.1
    Issue: PORTAL-138
    Change-Id: I49bfb2254ea4f2cb25a1fc6707df0420d205ee8b
    Signed-off-by: Christopher Lott (cl778h) <clott@research.att.com>

--- boot/portal_install.sh
+++ boot/portal_install.sh
@@ -68,7 +68,7 @@ apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 apt-get install -y --allow-unauthenticated docker-engine
 
 mkdir /opt/docker
-curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /opt/docker/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > /opt/docker/docker-compose
 chmod +x /opt/docker/docker-compose
 
 # Set the MTU size of docker containers to the minimum MTU size supported by vNICs. OpenStack deployments may need to know the external DNS IP
