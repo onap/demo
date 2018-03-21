dae3488a08202f32290bdd08798ca338b0086254
Marco Platania <platania@research.att.com>
2018-03-21 17:56:55 -0400

    Fix an issue that prevents vDNS replies
    - Replace vLB public IP with private IP towards the packet gen network
    
    Change-Id: If7b4d2ffe4891d9d856a62cc2d41dee1fb4ce397
    Issue-ID: INT-447
    Signed-off-by: Marco Platania <platania@research.att.com>

--- vnfs/vLBMS/scripts/set_gre_tunnel.sh
+++ vnfs/vLBMS/scripts/set_gre_tunnel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-LB_PUBLIC_IP=$(cat /opt/config/lb_public_ipaddr.txt)
+LB_TO_PKTGEN_IF=$(cat /opt/config/lb_to_pktgen_if.txt)
 LB_PRIVATE_IP=$(cat /opt/config/lb_private_ipaddr.txt)
 MY_PRIVATE_IP=$(cat /opt/config/local_private_ipaddr.txt)
 
-sed -i "s/x.x.x.x/"$LB_PUBLIC_IP"/g" /etc/bind/named.conf.options
+sed -i "s/x.x.x.x/"$LB_TO_PKTGEN_IF"/g" /etc/bind/named.conf.options
 
 ip tunnel add gre123 mode gre remote $LB_PRIVATE_IP local $MY_PRIVATE_IP ttl 255
 ip link set gre123 up
-ip addr add $LB_PUBLIC_IP"/32" dev gre123
+ip addr add $LB_TO_PKTGEN_IF"/32" dev gre123
 route add default dev gre123
 ifconfig eth0 down
 
