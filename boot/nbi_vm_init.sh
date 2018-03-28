3a8dda58039481f856615ba902f94765c4195eef
Rene Robert <rene.robert@orange.com>
2018-03-28 12:25:41 +0000

    add externalAPI/NBI project Issue-ID: INT-456
    Change-Id: Ib61780eace2d204f1a3763f497943540cfb33889
    Signed-off-by: Rene Robert <rene.robert@orange.com>

---
+++ boot/nbi_vm_init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NEXUS_USERNAME=$(cat /opt/config/nexus_username.txt)
+NEXUS_PASSWD=$(cat /opt/config/nexus_password.txt)
+NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt)
+DOCKER_IMAGE_VERSION=$(cat /opt/config/docker_version.txt)
