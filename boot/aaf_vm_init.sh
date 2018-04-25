ae730783f3806122c93a238ca73c695b25cac881
Instrumental <jcgmisc@stl.gathman.org>
2018-04-25 13:09:08 -0500

    Add AAF Installation Steps
    Issue-ID: AAF-256
    Change-Id: I2f1e9f4f4e98014ea6af95766fe41837471c0acc
    Signed-off-by: Instrumental <jcgmisc@stl.gathman.org>

--- boot/aaf_vm_init.sh
+++ boot/aaf_vm_init.sh
@@ -4,25 +4,54 @@ NEXUS_USERNAME=$(cat /opt/config/nexus_username.txt)
 NEXUS_PASSWD=$(cat /opt/config/nexus_password.txt)
 NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt)
 DOCKER_IMAGE_VERSION=$(cat /opt/config/docker_version.txt)
-LOCAL_IP=$(cat /opt/config/local_ip.txt)
-CASSANDRA_CONTAINER_NAME="aaf_cass"
+HOSTNAME=`hostname -a`
+FQDN=aaf.simpledemo.onap.org
+HOST_IP=$(cat /opt/config/dns_ip_addr.txt)
 
 docker login -u $NEXUS_USERNAME -p $NEXUS_PASSWD $NEXUS_DOCKER_REPO
 
 cd /opt/authz
 git pull
 
-cd /opt/authz/auth/auth-cass/docker
-./dinstall.sh
-sleep 2
+if [ "`docker container ls | grep aaf_cass`" = "" ]; then
+  # Cassandra Install
+  cd /opt/authz/auth/auth-cass/docker
+  # Phase 1
+  /bin/sh ./dinstall.sh
+  echo "Waiting for Cassandra to startup"
+  sleep 15
+  # Phase 2
+  if [ "`docker container ls | grep aaf_cass`" = "" ]; then
+      echo "Error on Cassandra install"
+      exit
+  else
+      /bin/sh ./dinstall.sh
+  fi
+fi
 
-#Update configuration file
-CASSANDRA_CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CASSANDRA_CONTAINER_NAME)
+CASS_IP=`docker inspect aaf_cass | grep '"IPAddress' | head -1 | cut -d '"' -f 4`
+CASS_HOST="cass.aaf.osaaf.org:"$CASS_IP
+
 sed -i "s/DOCKER_REPOSITORY=.*/DOCKER_REPOSITORY="$NEXUS_DOCKER_REPO"/g" /opt/authz/auth/docker/d.props
-sed -i "s/VERSION=.*/VERSION="$DOCKER_IMAGE_VERSION"/g" /opt/authz/auth/docker/d.props
-sed -i "s/HOSTNAME=.*/HOSTNAME="$(hostname)"/g" /opt/authz/auth/docker/d.props
-sed -i "s/HOST_IP=.*/HOST_IP="$LOCAL_IP"/g" /opt/authz/auth/docker/d.props
-sed -i "s/CASS_HOST=cass.aaf.osaaf.org:.*/CASS_HOST=cass.aaf.osaaf.org:"$CASSANDRA_CONTAINER_IP"/g" /opt/authz/auth/docker/d.props
+#sed -i "s/VERSION=.*/VERSION="$DOCKER_IMAGE_VERSION"/g" /opt/authz/auth/docker/d.props
+sed -i "s/HOSTNAME=.*/HOSTNAME="$HOSTNAME"/g" /opt/authz/auth/docker/d.props
+sed -i "s/HOST_IP=.*/HOST_IP="$HOST_IP"/g" /opt/authz/auth/docker/d.props
+sed -i "s/CASS_HOST=.*/CASS_HOST="$CASS_HOST"/g" /opt/authz/auth/docker/d.props
+
+if [ ! -e "/opt/app/osaaf/etc" ]; then
+  # Nothing installed, install sample
+  cp -Rf /opt/authz/auth/sample/* /opt/app/osaaf
+fi
+# Set Location information
+# Need new Deployment system properties
+CADI_LATITUDE=37.781
+CADI_LONGITUDE=-122.261
+
+CADI_TRUST_MASKS="${HOST_IP%\.[0-9]*}\\/24,${CASS_IP%\.[0-9]*}\\/24"
+sed -i "s/cadi_latitude=.*/cadi_latitude="$CADI_LATITUDE"/g" /opt/app/osaaf/local/org.osaaf.location.props
+sed -i "s/cadi_longitude=.*/cadi_longitude="$CADI_LONGITUDE"/g" /opt/app/osaaf/local/org.osaaf.location.props
+sed -i "s/cadi_registration_hostname=.*/cadi_registration_hostname="$FQDN"/g" /opt/app/osaaf/local/org.osaaf.location.props
+sed -i "s/cadi_trust_masks=.*/cadi_trust_masks="$CADI_TRUST_MASKS"/g" /opt/app/osaaf/local/org.osaaf.location.props
 
 cd /opt/authz/auth/docker
-./drun.sh
+/bin/sh ./dbuild.sh
