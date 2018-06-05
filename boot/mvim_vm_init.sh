f12be57098edab097efdc97622ccd552a2007363
System Administrator <root@lxinhui-m02.vmware.com>
2018-06-06 06:51:14 +0800

    Change release version
    This patch is to change the release version of multi vim
    
    Change-Id: Ib1c97c61f2697d59ee8cc58d22558cb4667f2875
    Issue-ID: MULTICLOUD-241
    Signed-off-by: Xinhui Li <lxinhui@vmware.com>

--- boot/mvim_vm_init.sh
+++ boot/mvim_vm_init.sh
@@ -13,7 +13,7 @@ source /opt/config/onap_ips.txt
 docker login -u $NEXUS_USERNAME -p $NEXUS_PASSWD $NEXUS_DOCKER_REPO
 docker pull $NEXUS_DOCKER_REPO/onap/multicloud/framework:$DOCKER_IMAGE_VERSION
 docker pull $NEXUS_DOCKER_REPO/onap/multicloud/vio:$DOCKER_IMAGE_VERSION
-docker pull $NEXUS_DOCKER_REPO/onap/multicloud/openstack-ocata:$DOCKER_IMAGE_OPENSTACK_VERSION
+docker pull $NEXUS_DOCKER_REPO/onap/multicloud/openstack/openstack-ocata:$DOCKER_IMAGE_OPENSTACK_VERSION
 docker pull $NEXUS_DOCKER_REPO/onap/multicloud/openstack-windriver:$DOCKER_IMAGE_OPENSTACK_VERSION
 
 docker rm -f multicloud-broker
