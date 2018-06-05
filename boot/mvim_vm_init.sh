00d7ad6122d396b74da01f53bfd5f6aab485de17
System Administrator <root@lxinhui-m02.vmware.com>
2018-06-06 06:51:14 +0800

    Change release version
    This patch is to change the release version of multi vim
    
    Change-Id: Ib1c97c61f2697d59ee8cc58d22558cb4667f2875
    Issue-ID: MULTICLOUD-241
    Signed-off-by: Xinhui Li <lxinhui@vmware.com>

--- boot/mvim_vm_init.sh
+++ boot/mvim_vm_init.sh
@@ -13,7 +13,7 @@ source /opt/config/onap_ips.txt
 docker login -u $NEXUS_USERNAME -p $NEXUS_PASSWD $NEXUS_DOCKER_REPO
 docker pull $NEXUS_DOCKER_REPO/onap/multicloud/framework:$DOCKER_IMAGE_VERSION
 docker pull $NEXUS_DOCKER_REPO/onap/multicloud/vio:$DOCKER_IMAGE_VERSION
-docker pull $NEXUS_DOCKER_REPO/onap/multicloud/openstack-ocata:$DOCKER_IMAGE_OPENSTACK_VERSION
+docker pull $NEXUS_DOCKER_REPO/onap/multicloud/openstack/openstack-ocata:$DOCKER_IMAGE_OPENSTACK_VERSION
 docker pull $NEXUS_DOCKER_REPO/onap/multicloud/openstack-windriver:$DOCKER_IMAGE_OPENSTACK_VERSION
 
 docker rm -f multicloud-broker
@@ -23,5 +23,5 @@ docker rm -f multicloud-windriver
 
 docker run -d -t -e MSB_ADDR=$OPENO_IP -e AAI_ADDR=$AAI_IP1 -p 9001:9001 --name multicloud-broker $NEXUS_DOCKER_REPO/onap/multicloud/framework:$DOCKER_IMAGE_VERSION
 docker run -d -t -e MSB_ADDR=$OPENO_IP -e AAI_ADDR=$AAI_IP1 -e MR_ADDR=$MR_IP -p 9004:9004 --name multicloud-vio $NEXUS_DOCKER_REPO/onap/multicloud/vio:$DOCKER_IMAGE_VERSION
-docker run -d -t -e MSB_ADDR=$OPENO_IP -e AAI_ADDR=$AAI_IP1 -p 9006:9006 --name multicloud-ocata $NEXUS_DOCKER_REPO/onap/multicloud/openstack-ocata:$DOCKER_IMAGE_OPENSTACK_VERSION
+docker run -d -t -e MSB_ADDR=$OPENO_IP -e AAI_ADDR=$AAI_IP1 -p 9006:9006 --name multicloud-ocata $NEXUS_DOCKER_REPO/onap/multicloud/openstack/openstack-ocata:$DOCKER_IMAGE_OPENSTACK_VERSION
 docker run -d -t -e MSB_ADDR=$OPENO_IP -e AAI_ADDR=$AAI_IP1 -p 9005:9005 --name multicloud-windriver $NEXUS_DOCKER_REPO/onap/multicloud/openstack-windriver:$DOCKER_IMAGE_OPENSTACK_VERSION
