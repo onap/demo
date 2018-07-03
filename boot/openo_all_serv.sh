08c32f2308eba6488f49389bcd83b593583adc7e
AbirATTAR <abir.el_attar@nokia.com>
2018-07-03 09:28:58 +0200

    Improve the way to deploy onap via proxy
    Issue-ID: OOM-1275
    Change-Id: I0d77bcfe1e9ca183b3ba1aa56fff713db33b677b
    Signed-off-by: Abir EL ATTAR <abir.el_attar@nokia.com>

--- boot/openo_all_serv.sh
+++ boot/openo_all_serv.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+HTTP_PROXY=$(cat /opt/config/http_proxy.txt)
+HTTPS_PROXY=$(cat /opt/config/https_proxy.txt)
+
+if [ $HTTP_PROXY != " " ]
+then
+    export http_proxy=$HTTP_PROXY
+    export https_proxy=$HTTPS_PROXY
+fi
 
 bash /opt/msb_vm_init.sh &>/dev/null &disown
 bash /opt/vnfsdk_vm_init.sh &>/dev/null &disown
