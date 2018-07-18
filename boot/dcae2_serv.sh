1f5ef678b4298fdef56e168dd802627a80f6f441
gwu <gary.i.wu@huawei.com>
2018-07-17 22:36:47 -0700

    Bug fix on HEAT template HTTP_PROXY
    Various boot scripts should have been checking for "no_proxy"
    but were checking for " " instead.  This fixes the issue.
    
    Also add default values for HTTP_PROXY variables.
    
    Change-Id: I5245018a7bb9c33b84ac3096d8a9c0986b2e976c
    Issue-ID: OOM-1275
    Signed-off-by: gwu <gary.i.wu@huawei.com>

--- boot/dcae2_serv.sh
+++ boot/dcae2_serv.sh
@@ -28,7 +28,7 @@
 HTTP_PROXY=$(cat /opt/config/http_proxy.txt)
 HTTPS_PROXY=$(cat /opt/config/https_proxy.txt)
 
-if [ $HTTP_PROXY != " " ]
+if [ $HTTP_PROXY != "no_proxy" ]
 then
     export http_proxy=$HTTP_PROXY
     export https_proxy=$HTTPS_PROXY
