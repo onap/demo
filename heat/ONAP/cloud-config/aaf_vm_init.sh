71429b013013f24f6545226e9b1d6a997223c59d
Instrumental <jonathan.gathman@att.com>
2018-08-16 17:16:56 -0500

    Adjust aaf HEAT shs
    Issue-ID: AAF-419
    Change-Id: Id4fbce7fb25c703a74f93afbbb519b980d5b96dd
    Signed-off-by: Instrumental <jonathan.gathman@att.com>

--- heat/ONAP/cloud-config/aaf_vm_init.sh
+++ heat/ONAP/cloud-config/aaf_vm_init.sh
@@ -2,54 +2,69 @@
 
 NEXUS_USERNAME=$(cat /opt/config/nexus_username.txt)
 NEXUS_PASSWD=$(cat /opt/config/nexus_password.txt)
-NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt)
-DOCKER_IMAGE_VERSION=$(cat /opt/config/docker_version.txt)
+NEXUS_DOCKER_REPO=`grep "DOCKER_REPOSITORY=" /opt/authz/auth/docker/d.props`
+if [ "$NEXUS_DOCKER_REPO" = "DOCKER_REPOSITORY=" ]; then
+  NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt)
+else
+  NEXUS_DOCKER_REPO=${NEXUS_DOCKER_REPO#DOCKER_REPOSITORY=}
+fi
+
 HOSTNAME=`hostname`
 FQDN=aaf.api.simpledemo.onap.org
 HOST_IP=$(cat /opt/config/local_ip.txt)
 
 docker login -u $NEXUS_USERNAME -p $NEXUS_PASSWD $NEXUS_DOCKER_REPO
 
-cd /opt/authz
-git pull
-
+if [ -e "/opt/authz" ]; then
+  cd /opt/authz
+  git pull
+else
+  cd /opt
+  git clone https://gerrit.onap.org/r/aaf/authz
+  cd authz
+fi
 
 cd /opt/authz/auth/auth-cass/docker
 if [ "`docker container ls | grep aaf_cass`" = "" ]; then
   # Cassandra Install
   echo Phase 1 Cassandra Install
-  ./dinstall.sh
+  bash ./dinstall.sh
 fi
 
+VERSION=$(grep VERSION /opt/authz/auth/docker/d.props.init)
+VERSION=${VERSION#VERSION=}
 CASS_IP=`docker inspect aaf_cass | grep '"IPAddress' | head -1 | cut -d '"' -f 4`
 CASS_HOST="cass.aaf.osaaf.org:"$CASS_IP
+sed -i "s/CASS_HOST=.*/CASS_HOST="$CASS_HOST"/g" /opt/authz/auth/docker/cass.props
+# TODO Pull from Config Dir
+CADI_LATITUDE=37.781
+CADI_LONGITUDE=-122.261
 
 sed -i "s/DOCKER_REPOSITORY=.*/DOCKER_REPOSITORY="$NEXUS_DOCKER_REPO"/g" /opt/authz/auth/docker/d.props
-#sed -i "s/VERSION=.*/VERSION="$DOCKER_IMAGE_VERSION"/g" /opt/authz/auth/docker/d.props
+sed -i "s/VERSION=.*/VERSION="$VERSION"/g" /opt/authz/auth/docker/d.props
 sed -i "s/HOSTNAME=.*/HOSTNAME="$HOSTNAME"/g" /opt/authz/auth/docker/d.props
 sed -i "s/HOST_IP=.*/HOST_IP="$HOST_IP"/g" /opt/authz/auth/docker/d.props
-sed -i "s/CASS_HOST=.*/CASS_HOST="$CASS_HOST"/g" /opt/authz/auth/docker/d.props
+sed -i "s/CADI_LATITUDE=.*/CADI_LATITUDE="$CADI_LATITUDE"/g" /opt/authz/auth/docker/d.props
+sed -i "s/CASS_LONGITUDE=.*/CADI_LONGITUDE="$CADI_LONGITUDE"/g" /opt/authz/auth/docker/d.props
+# Set Location information
+# Need new Deployment system properties
 
 if [ ! -e "/opt/app/osaaf/etc" ]; then
   # Nothing installed, install sample
   mkdir -p /opt/app/osaaf/logs
   cd /opt/app/osaaf/logs
-  mkdir fs cm gui hello locate oauth service
+  mkdir -p fs cm gui hello locate oauth service
   cd /opt
   cp -Rf /opt/authz/auth/sample/* /opt/app/osaaf
 fi
-# Set Location information
-# Need new Deployment system properties
-CADI_LATITUDE=37.781
-CADI_LONGITUDE=-122.261
-
-CADI_TRUST_MASKS="${HOST_IP%\.[0-9]*}\\/24,${CASS_IP%\.[0-9]*}\\/24"
-sed -i "s/cadi_latitude=.*/cadi_latitude="$CADI_LATITUDE"/g" /opt/app/osaaf/local/org.osaaf.location.props
-sed -i "s/cadi_longitude=.*/cadi_longitude="$CADI_LONGITUDE"/g" /opt/app/osaaf/local/org.osaaf.location.props
-sed -i "s/cadi_registration_hostname=.*/cadi_registration_hostname="$FQDN"/g" /opt/app/osaaf/local/org.osaaf.location.props
-sed -i "s/cadi_trust_masks=.*/cadi_trust_masks="$CADI_TRUST_MASKS"/g" /opt/app/osaaf/local/org.osaaf.location.props
 
 cd /opt/authz/auth/docker
-./dbuild.sh
-sleep 5
-./drun.sh
+#
+# Build AAF config volume
+#./dbuild.sh
+#sleep 5
+
+# Create or check Credentials on startup
+bash ./aaf.sh
+
+bash ./drun.sh
