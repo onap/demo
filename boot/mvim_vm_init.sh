6d5e5bb4b1ce3a51cf8b05a86f4d411d8fc21875
Haibin Huang <haibin.huang@intel.com>
2019-01-24 09:53:58 +0800

    Add starlingx plugin for demo
    Change-Id: Id66c41fe0de0708f5c2dc0b04026ca923fd70eb4
    Issue-ID: MULTICLOUD-428
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>

--- boot/mvim_vm_init.sh
+++ boot/mvim_vm_init.sh
@@ -16,15 +16,18 @@ docker pull $NEXUS_DOCKER_REPO/onap/multicloud/vio:$DOCKER_IMAGE_VERSION
 docker pull $NEXUS_DOCKER_REPO/onap/multicloud/openstack-ocata:$DOCKER_IMAGE_OPENSTACK_VERSION
 docker pull $NEXUS_DOCKER_REPO/onap/multicloud/openstack-pike:$DOCKER_IMAGE_OPENSTACK_VERSION
 docker pull $NEXUS_DOCKER_REPO/onap/multicloud/openstack-windriver:$DOCKER_IMAGE_OPENSTACK_VERSION
+docker pull $NEXUS_DOCKER_REPO/onap/multicloud/openstack-starlingx:$DOCKER_IMAGE_OPENSTACK_VERSION
 
 docker rm -f multicloud-broker
 docker rm -f multicloud-vio
 docker rm -f multicloud-ocata
 docker rm -f multicloud-pike
 docker rm -f multicloud-windriver
+docker rm -f multicloud-starlingx
 
 docker run -d -t -e MSB_ADDR=$MULTISERV_IP -e AAI_ADDR=$AAI_IP1 -p 9001:9001 --name multicloud-broker $NEXUS_DOCKER_REPO/onap/multicloud/framework:$DOCKER_IMAGE_VERSION
 docker run -d -t -e MSB_ADDR=$MULTISERV_IP -e AAI_ADDR=$AAI_IP1 -e MR_ADDR=$MR_IP -p 9004:9004 --name multicloud-vio $NEXUS_DOCKER_REPO/onap/multicloud/vio:$DOCKER_IMAGE_VERSION
 docker run -d -t -e MSB_ADDR=$MULTISERV_IP -e AAI_ADDR=$AAI_IP1 -p 9006:9006 --name multicloud-ocata $NEXUS_DOCKER_REPO/onap/multicloud/openstack-ocata:$DOCKER_IMAGE_OPENSTACK_VERSION
 docker run -d -t -e MSB_ADDR=$MULTISERV_IP -e AAI_ADDR=$AAI_IP1 -p 9007:9007 --name multicloud-pike $NEXUS_DOCKER_REPO/onap/multicloud/openstack-pike:$DOCKER_IMAGE_OPENSTACK_VERSION
 docker run -d -t -e MSB_ADDR=$MULTISERV_IP -e AAI_ADDR=$AAI_IP1 -p 9005:9005 --name multicloud-windriver $NEXUS_DOCKER_REPO/onap/multicloud/openstack-windriver:$DOCKER_IMAGE_OPENSTACK_VERSION
+docker run -d -t -e MSB_ADDR=$MULTISERV_IP -e AAI_ADDR=$AAI_IP1 -p 9009:9009 --name multicloud-starlingx $NEXUS_DOCKER_REPO/onap/multicloud/openstack-starlingx:$DOCKER_IMAGE_OPENSTACK_VERSION
