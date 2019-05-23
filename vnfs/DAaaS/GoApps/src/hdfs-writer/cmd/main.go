1e28945a7d45de4f2b092112f0b2b8f6d166b6b4
Rajamohan Raj <rajamohan.raj@intel.com>
2019-05-23 12:47:06 -0700

    Sample file reading from hdfs.
    Just a sample go file for reading from a hdfs file.
    Shall be deleted later.
    
    Issue-ID: ONAPARC-451
    Signed-off-by: Rajamohan Raj <rajamohan.raj@intel.com>
    Change-Id: Icfdd4b2196e080b6da45dadaf498eeb5d5fba1ac

--- vnfs/DAaaS/GoApps/src/hdfs-writer/cmd/main.go
+++ vnfs/DAaaS/GoApps/src/hdfs-writer/cmd/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"log"
+	"github.com/colinmarc/hdfs/v2"
 )
 
 func main() {
 	log.Println("Starting the HDFS writer")
-	fmt.Println("Starting the HDFS writer ...")
+	localSourceFile := os.Args[1]
+	hdfsDestination := os.Args[2]
+
+	log.Println("localSourceFile:: "+localSourceFile)
+	log.Println("hdfsDestination:: "+hdfsDestination)
+
+	client, _ := hdfs.New("hdfs://hdfs-1-namenode-1.hdfs-1-namenode.hdfs1.svc.cluster.local:8020")
+	file, _ := client.Open("/kafka.txt")
+
+	buf := make([]byte, 59)
+	file.ReadAt(buf, 48847)
+	fmt.Println(string(buf))
+
 }
