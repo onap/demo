fe675bc54edf016080edad91d648140668e96253
Rajamohan Raj <rajamohan.raj@intel.com>
2019-05-30 23:33:35 +0000

    Refactor promql_api after project structure change
    Refactored prompql_api after project structure change.
    Fix review comments and make a better read me.
    
    Change-Id: I40b7b2da40d85e9f297d4bb87d8633c71bdd1ec0
    Issue-ID: ONAPARC-452
    Signed-off-by: Rajamohan Raj <rajamohan.raj@intel.com>

--- vnfs/DAaaS/lib/promql_api/prom_ql_api.py
+++ vnfs/DAaaS/lib/promql_api/prom_ql_api.py
@@ -1,23 +1,20 @@
 from __future__ import print_function
 from os import environ
 import logging
-import sysconfig
 import requests
 from requests.exceptions import HTTPError
 
 
-#LABELS = [ 'irate(http_requests_total{code="200"}[1m])', 'collectd_cpu_percent{job="collectd", exported_instance="an11-31"}[1m]' ]
-#LABELS = ['irate(collectd_cpufreq{exported_instance="otconap7",cpufreq="1"}[2m])', 'go_info']
+
 API_VERSION = '/api/v1/query'
-LIST_OF_ENV_VARIABLES = ["PROMETHEUS_SERVER_URL"]
+LIST_OF_ENV_VARIABLES = ["DATA_ENDPOINT"]
 MAP_ENV_VARIABLES = dict()
-#MAP_ENV_VARIABLES['PROMETHEUS_SERVER_URL']='http://172.25.103.1:30090' #to be deleted
 LOG = logging.getLogger(__name__)
 
 def set_log_config():
-    logging.basicConfig(format='%(asctime)s ::%(filename)s :: %(funcName)s :: %(levelname)s :: %(message)s', 
+    logging.basicConfig(format='%(asctime)s ::%(filename)s :: %(funcName)s :: %(levelname)s :: %(message)s',
                     datefmt='%m-%d-%Y %I:%M:%S%p',
-                    level=logging.DEBUG, 
+                    level=logging.DEBUG,
                     filename='promql_api.log',
                     filemode='w')
     LOG.info("Set the log configs.")
@@ -30,27 +27,38 @@ def load_and_validate_env_vars(list_of_env_vars):
             LOG.info("Found env variable: {} ".format(env_var.upper()))
             MAP_ENV_VARIABLES[env_var.upper()] = environ.get(env_var)
         else:
-            #MAP_ENV_VARIABLES['PROMETHEUS_SERVER_URL']='http://172.25.103.1:30090' # to be deleted
+            #MAP_ENV_VARIABLES['DATA_ENDPOINT']='http://127.0.0.1:30090' # to be deleted
             LOG.error("Env var: {} not found ! ".format(env_var.upper()))
             raise KeyError("Env variable: {} not found ! ".format(env_var.upper()))
 
 
-def query_m3db(LABELS):
+def query(LABELS):
     """
     Input parameters:
-        LABELS : a list of the LABELS
+        LABELS : a list of the LABELS like ['irate(collectd_cpufreq{exported_instance="otconap7",cpufreq="1"}[2m])']
     Return:
-        returns a list of  result sets of different labels
+        returns a list of  result sets of different labels.
+        SAMPLE O/P:
+        [{'metric': {'cpufreq': '1',
+             'endpoint': 'collectd-prometheus',
+             'exported_instance': 'otconap7',
+             'instance': '172.25.103.1:9103',
+             'job': 'collectd',
+             'namespace': 'edge1',
+             'pod': 'plundering-liger-collectd-wz7xg',
+             'service': 'collectd'},
+        'value': [1559177169.415, '119727200']}]
     """
-
+    set_log_config()
+    load_and_validate_env_vars(LIST_OF_ENV_VARIABLES)
     LOG.info("Forming the get request ...")
     list_of_substrings = []
     params_map = {}
     list_of_result_sets = []
-    list_of_substrings.append(MAP_ENV_VARIABLES['PROMETHEUS_SERVER_URL'])
+    list_of_substrings.append(MAP_ENV_VARIABLES['DATA_ENDPOINT'])
     list_of_substrings.append(API_VERSION)
     url = ''.join(list_of_substrings)
-    
+
     for each_label in LABELS:
         params_map['query'] = each_label
         try:
@@ -60,24 +68,15 @@ def query_m3db(LABELS):
             response.raise_for_status()
         except HTTPError as http_err:
             print(f'HTTP error occurred: {http_err}')
-            return None  
+            return None
         except Exception as err:
             print(f'Other error occurred: {err}')
-            return None 
+            return None
         else:
-            
+
             results = response.json()['data']['result']
             LOG.info('::::::::::RESULTS::::::::::::: {}'.format(each_label))
             for each_result in results:
                 LOG.info(each_result)
             list_of_result_sets.append(results)
     return list_of_result_sets
-
-
-# def main():
-#     set_log_config()
-#     load_and_validate_env_vars(LIST_OF_ENV_VARIABLES)
-#     query_m3db(LABELS)
-
-# if __name__ == "__main__":
-#     main()
--- vnfs/DAaaS/sample-apps/m3db_promql/sample_promql_query.py
+++ vnfs/DAaaS/sample-apps/m3db_promql/sample_promql_query.py
@@ -1,10 +1,14 @@
-from promql_api.prom_ql_api import query_m3db
+from promql_api.prom_ql_api import query
 import pprint
 
 LABELS = ['irate(collectd_cpufreq{exported_instance="otconap7",cpufreq="1"}[2m])']
 
+#Other examples
+#LABELS = [ 'irate(http_requests_total{code="200"}[1m])', 'collectd_cpu_percent{job="collectd", exported_instance="an11-31"}[1m]' ]
+#LABELS = ['irate(collectd_cpufreq{exported_instance="otconap7",cpufreq="1"}[2m])', 'go_info']
+
 def main():
-    list_of_result_sets = query_m3db(LABELS)
+    list_of_result_sets = query(LABELS)
     for each_result in list_of_result_sets:
         pprint.pprint(each_result)
 
