76e1bfd42d31e71b3dda12de842f60b2fc3b4b94
Rajamohan Raj <rajamohan.raj@intel.com>
2019-06-20 10:40:13 -0700

    Add support for query_range in promql_api
    Added support for query_range in promql_api
    
    Issue-ID: ONAPARC-452
    Signed-off-by: Rajamohan Raj <rajamohan.raj@intel.com>
    Change-Id: If4870c8a8ea2fd4e6b23237cb8fd0f34a17fe3e0

--- vnfs/DAaaS/sample-apps/m3db_promql/build/sample_promql_query.py
+++ vnfs/DAaaS/sample-apps/m3db_promql/build/sample_promql_query.py
@@ -17,19 +17,32 @@
 
 
 from promql_api.prom_ql_api import query
+from promql_api.prom_ql_api import query_range
 import pprint
 
 QUERY_STRING = ['irate(collectd_cpufreq{exported_instance="otconap7",cpufreq="1"}[2m])']
+MAP_OF_PARAMETERS = {'query': 'up', 'start': '2019-06-19T20:10:30.781Z', 'end': '2019-06-19T20:10:45.781Z', 'step': '15s'}
 
 #Other examples
 #QUERY_STRING = [ 'irate(http_requests_total{code="200"}[1m])', 'collectd_cpu_percent{job="collectd", exported_instance="otconap7"}[1m]' ]
 #QUERY_STRING = ['irate(collectd_cpufreq{exported_instance="otconap7",cpufreq="1"}[2m])', 'go_info']
+#MAP_OF_PARAMETERS = {'query': 'up', 'start': '2019-06-19T20:10:30.781Z', 'end': '2019-06-19T20:10:45.781Z', 'step': '15s', 'timeout':'600s'}
+
+def demo_query():
+        list_of_result_sets = query(QUERY_STRING)
+        if list_of_result_sets:
+                for each_result in list_of_result_sets:
+                    pprint.pprint(each_result)
+
+def demo_query_range():
+        list_of_result_sets = query_range(MAP_OF_PARAMETERS)
+        pprint.pprint(list_of_result_sets)
+
 
 def main():
-    list_of_result_sets = query(QUERY_STRING)
-    if list_of_result_sets:
-        for each_result in list_of_result_sets:
-                pprint.pprint(each_result)
+        demo_query()
+        demo_query_range()
 
+    
 if __name__ == "__main__":
-     main()
+        main()
