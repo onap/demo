787f229616b7d26d4a712288018b83c8de6d900c
Dileep Ranganathan <dileep.ranganathan@intel.com>
2019-08-07 00:14:07 -0700

    Collectd Global CRD
    This patch introduces an enhancement to the existing Collectd Operator.
    It has now 2 CRDS CollectdGlobal and CollectdPlugin and corresponding
    controllers. CollectdGlobal is used to specify the global options of
    collectd conf and CollectdPlugin is used to load/unload plugins.
    
    Added synchronization when both controllers try to fetch the same
    resources. Refactored some of the common code to collectdutils.
    
    Issue-ID: ONAPARC-461
    Signed-off-by: Dileep Ranganathan <dileep.ranganathan@intel.com>
    Change-Id: Id2edf6fa6de56d9d7216ebb258b3017661c6a3dd

--- vnfs/DAaaS/microservices/collectd-operator/pkg/controller/collectdglobal/collectdglobal_controller.go
+++ vnfs/DAaaS/microservices/collectd-operator/pkg/controller/collectdglobal/collectdglobal_controller.go
@@ -1,12 +1,14 @@
-package collectdplugin
+package collectdglobal
 
 import (
 	"context"
 	"fmt"
+	//"path/filepath"
 	"reflect"
 	"strings"
 
 	"github.com/go-logr/logr"
+	"github.com/operator-framework/operator-sdk/pkg/predicate"
 
 	onapv1alpha1 "demo/vnfs/DAaaS/microservices/collectd-operator/pkg/apis/onap/v1alpha1"
 	collectdutils "demo/vnfs/DAaaS/microservices/collectd-operator/pkg/controller/utils"
@@ -16,6 +18,7 @@ import (
 	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/retry"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -26,10 +29,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
-var log = logf.Log.WithName("controller_collectdplugin")
+var log = logf.Log.WithName("controller_collectdglobal")
 
-
-// Add creates a new CollectdPlugin Controller and adds it to the Manager. The Manager will set fields on the Controller
+// Add creates a new CollectdGlobal Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
 	return add(mgr, newReconciler(mgr))
@@ -37,21 +39,21 @@ func Add(mgr manager.Manager) error {
 
 // newReconciler returns a new reconcile.Reconciler
 func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	return &ReconcileCollectdPlugin{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+	return &ReconcileCollectdGlobal{client: mgr.GetClient(), scheme: mgr.GetScheme()}
 }
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	// Create a new controller
-	log.V(1).Info("Creating a new controller for CollectdPlugin")
-	c, err := controller.New("collectdplugin-controller", mgr, controller.Options{Reconciler: r})
+	log.V(1).Info("Creating a new controller for CollectdGlobal")
+	c, err := controller.New("collectdglobal-controller", mgr, controller.Options{Reconciler: r})
 	if err != nil {
 		return err
 	}
 
-	// Watch for changes to primary resource CollectdPlugin
-	log.V(1).Info("Add watcher for primary resource CollectdPlugin")
-	err = c.Watch(&source.Kind{Type: &onapv1alpha1.CollectdPlugin{}}, &handler.EnqueueRequestForObject{})
+	// Watch for changes to primary resource CollectdGlobal
+	log.V(1).Info("Add watcher for primary resource CollectdGlobal")
+	err = c.Watch(&source.Kind{Type: &onapv1alpha1.CollectdGlobal{}}, &handler.EnqueueRequestForObject{}, predicate.GenerationChangedPredicate{})
 	if err != nil {
 		return err
 	}
@@ -60,24 +62,22 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	err = c.Watch(
 		&source.Kind{Type: &appsv1.DaemonSet{}},
 		&handler.EnqueueRequestsFromMapFunc{
-			ToRequests: handler.ToRequestsFunc(func (a handler.MapObject) []reconcile.Request {
+			ToRequests: handler.ToRequestsFunc(func(a handler.MapObject) []reconcile.Request {
 				labelSelector, err := collectdutils.GetWatchLabels()
 				labels := strings.Split(labelSelector, "=")
 				if err != nil {
 					log.Error(err, "Failed to get watch labels, continuing with default label")
 				}
-				rcp := r.(*ReconcileCollectdPlugin)
+				rcp := r.(*ReconcileCollectdGlobal)
 				// Select the Daemonset with labelSelector (Defautl  is app=collectd)
-				if a.Meta.GetLabels()[labels[0]] == labels[1]  {
+				if a.Meta.GetLabels()[labels[0]] == labels[1] {
 					var requests []reconcile.Request
-					cpList, err := collectdutils.GetCollectdPluginList(rcp.client, a.Meta.GetNamespace())
-					if err != nil {
+					cg, err := collectdutils.GetCollectdGlobal(rcp.client, a.Meta.GetNamespace())
+					if err != nil || cg == nil {
 						return nil
 					}
-					for _, cp := range cpList.Items {
-						requests = append(requests, reconcile.Request{
-							NamespacedName: client.ObjectKey{Namespace: cp.Namespace, Name: cp.Name}})
-					}
+					requests = append(requests, reconcile.Request{
+						NamespacedName: client.ObjectKey{Namespace: cg.Namespace, Name: cg.Name}})
 					return requests
 				}
 				return nil
@@ -90,43 +90,41 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	return nil
 }
 
+// blank assignment to verify that ReconcileCollectdGlobal implements reconcile.Reconciler
+var _ reconcile.Reconciler = &ReconcileCollectdGlobal{}
 
-// blank assignment to verify that ReconcileCollectdPlugin implements reconcile.Reconciler
-var _ reconcile.Reconciler = &ReconcileCollectdPlugin{}
-
-// ReconcileCollectdPlugin reconciles a CollectdPlugin object
-type ReconcileCollectdPlugin struct {
+// ReconcileCollectdGlobal reconciles a CollectdGlobal object
+type ReconcileCollectdGlobal struct {
 	// This client, initialized using mgr.Client() above, is a split client
 	// that reads objects from the cache and writes to the apiserver
 	client client.Client
 	scheme *runtime.Scheme
 }
 
-
-// Reconcile reads that state of the cluster for a CollectdPlugin object and makes changes based on the state read
-// and what is in the CollectdPlugin.Spec
+// Reconcile reads that state of the cluster for a CollectdGlobal object and makes changes based on the state read
+// and what is in the CollectdGlobal.Spec
 // TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
 // a Pod as an example
 // Note:
 // The Controller will requeue the Request to be processed again if the returned error is non-nil or
 // Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
-func (r *ReconcileCollectdPlugin) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+func (r *ReconcileCollectdGlobal) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
-	reqLogger.Info("Reconciling CollectdPlugin")
+	reqLogger.Info("Reconciling CollectdGlobal")
 
-	// Fetch the CollectdPlugin instance
-	instance := &onapv1alpha1.CollectdPlugin{}
+	// Fetch the CollectdGlobal instance
+	instance := &onapv1alpha1.CollectdGlobal{}
 	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
 	if err != nil {
 		if errors.IsNotFound(err) {
 			// Request object not found, could have been deleted after reconcile request.
 			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
 			// Return and don't requeue
-			reqLogger.V(1).Info("CollectdPlugin object Not found")
+			reqLogger.V(1).Info("CollectdGlobal object Not found")
 			return reconcile.Result{}, nil
 		}
 		// Error reading the object - requeue the request.
-		reqLogger.V(1).Info("Error reading the CollectdPlugin object, Requeuing")
+		reqLogger.V(1).Info("Error reading the CollectdGlobal object, Requeuing")
 		return reconcile.Result{}, err
 	}
 
@@ -143,18 +141,18 @@ func (r *ReconcileCollectdPlugin) Reconcile(request reconcile.Request) (reconcil
 		}
 		return reconcile.Result{}, nil
 	}
-	// Handle the reconciliation for CollectdPlugin.
-	// At this stage the Status of the CollectdPlugin should NOT be ""
-	err = r.handleCollectdPlugin(reqLogger, instance, false)
+	// Handle the reconciliation for CollectdGlobal.
+	// At this stage the Status of the CollectdGlobal should NOT be ""
+	err = r.handleCollectdGlobal(reqLogger, instance, false)
 	return reconcile.Result{}, err
 }
 
-// handleCollectdPlugin regenerates the collectd conf on CR Create, Update, Delete events
-func (r *ReconcileCollectdPlugin) handleCollectdPlugin(reqLogger logr.Logger, cr *onapv1alpha1.CollectdPlugin, isDelete bool) error {
+// handleCollectdGlobal regenerates the collectd conf on CR Create, Update, Delete events
+func (r *ReconcileCollectdGlobal) handleCollectdGlobal(reqLogger logr.Logger, cr *onapv1alpha1.CollectdGlobal, isDelete bool) error {
 
 	rmap, err := collectdutils.FindResourceMapForCR(r.client, reqLogger, cr.Namespace)
 	if err != nil {
-		reqLogger.Error(err, "Skip reconcile: Resources not found")
+		reqLogger.Info(":::: Skip current reconcile:::: Resources not found. Cache might be stale. Requeue")
 		return err
 	}
 
@@ -162,7 +160,7 @@ func (r *ReconcileCollectdPlugin) handleCollectdPlugin(reqLogger logr.Logger, cr
 	reqLogger.V(1).Info("Found ResourceMap")
 	reqLogger.V(1).Info(":::: ConfigMap Info ::::", "ConfigMap.Namespace", cm.Namespace, "ConfigMap.Name", cm.Name)
 
-	collectdConf, err := collectdutils.RebuildCollectdConf(r.client, cr.Namespace, isDelete, cr.Spec.PluginName)
+	collectdConf, err := collectdutils.RebuildCollectdConf(r.client, cr.Namespace, isDelete, "")
 	if err != nil {
 		reqLogger.Error(err, "Skip reconcile: Rebuild conf failed")
 		return err
@@ -173,17 +171,22 @@ func (r *ReconcileCollectdPlugin) handleCollectdPlugin(reqLogger logr.Logger, cr
 	})
 
 	cm.Data["collectd.conf"] = collectdConf
-
-	// Update the ConfigMap with new Spec and reload DaemonSets
-	reqLogger.Info("Updating the ConfigMap", "ConfigMap.Namespace", cm.Namespace, "ConfigMap.Name", cm.Name)
-	log.V(1).Info("ConfigMap Data", "Map: ", cm.Data)
-	err = r.client.Update(context.TODO(), cm)
-	if err != nil {
-		reqLogger.Error(err, "Update the ConfigMap failed", "ConfigMap.Namespace", cm.Namespace, "ConfigMap.Name", cm.Name)
-		return err
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		// Update the ConfigMap with new Spec and reload DaemonSets
+		reqLogger.Info("Updating the ConfigMap", "ConfigMap.Namespace", cm.Namespace, "ConfigMap.Name", cm.Name)
+		log.V(1).Info("ConfigMap Data", "Map: ", cm.Data)
+		err = r.client.Update(context.TODO(), cm)
+		if err != nil {
+			reqLogger.Error(err, "Update the ConfigMap failed", "ConfigMap.Namespace", cm.Namespace, "ConfigMap.Name", cm.Name)
+			return err
+		}
+		return nil
+	})
+	if retryErr != nil {
+		panic(fmt.Errorf("Update failed: %v", retryErr))
 	}
 
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+	retryErr = retry.RetryOnConflict(retry.DefaultRetry, func() error {
 		// Retrieve the latest version of Daemonset before attempting update
 		// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
 		// Select DaemonSets with label
@@ -210,6 +213,7 @@ func (r *ReconcileCollectdPlugin) handleCollectdPlugin(reqLogger logr.Logger, cr
 		ds.Spec.Template.SetAnnotations(map[string]string{
 			"daaas-random": collectdutils.ComputeSHA256([]byte(collectdConf)),
 		})
+		r.handleAdditonalConfigMap(reqLogger, cr, ds)
 		updateErr := r.client.Update(context.TODO(), ds)
 		return updateErr
 	})
@@ -228,8 +232,8 @@ func (r *ReconcileCollectdPlugin) handleCollectdPlugin(reqLogger logr.Logger, cr
 }
 
 // Handle Delete CR event for additional cleanup
-func (r *ReconcileCollectdPlugin) handleDelete(reqLogger logr.Logger, cr *onapv1alpha1.CollectdPlugin) (bool, error) {
-	// Check if the CollectdPlugin instance is marked to be deleted, which is
+func (r *ReconcileCollectdGlobal) handleDelete(reqLogger logr.Logger, cr *onapv1alpha1.CollectdGlobal) (bool, error) {
+	// Check if the CollectdGlobal instance is marked to be deleted, which is
 	// indicated by the deletion timestamp being set.
 	isMarkedToBeDeleted := cr.GetDeletionTimestamp() != nil
 	if isMarkedToBeDeleted {
@@ -239,14 +243,14 @@ func (r *ReconcileCollectdPlugin) handleDelete(reqLogger logr.Logger, cr *onapv1
 		_ = r.client.Status().Update(context.TODO(), cr)
 
 		if collectdutils.Contains(cr.GetFinalizers(), collectdutils.CollectdFinalizer) {
-			// Run finalization logic for collectdPluginFinalizer. If the
+			// Run finalization logic for CollectdFinalizer. If the
 			// finalization logic fails, don't remove the finalizer so
 			// that we can retry during the next reconciliation.
-			if err := r.finalizeCollectdPlugin(reqLogger, cr); err != nil {
+			if err := r.finalizeCollectdGlobal(reqLogger, cr); err != nil {
 				return isMarkedToBeDeleted, err
 			}
 
-			// Remove collectdPluginFinalizer. Once all finalizers have been
+			// Remove CollectdFinalizer. Once all finalizers have been
 			// removed, the object will be deleted.
 			cr.SetFinalizers(collectdutils.Remove(cr.GetFinalizers(), collectdutils.CollectdFinalizer))
 			err := r.client.Update(context.TODO(), cr)
@@ -258,7 +262,7 @@ func (r *ReconcileCollectdPlugin) handleDelete(reqLogger logr.Logger, cr *onapv1
 	return isMarkedToBeDeleted, nil
 }
 
-func (r *ReconcileCollectdPlugin) updateStatus(cr *onapv1alpha1.CollectdPlugin) error {
+func (r *ReconcileCollectdGlobal) updateStatus(cr *onapv1alpha1.CollectdGlobal) error {
 	switch cr.Status.Status {
 	case onapv1alpha1.Initial:
 		cr.Status.Status = onapv1alpha1.Created
@@ -278,18 +282,18 @@ func (r *ReconcileCollectdPlugin) updateStatus(cr *onapv1alpha1.CollectdPlugin) 
 	return err
 }
 
-func (r *ReconcileCollectdPlugin) finalizeCollectdPlugin(reqLogger logr.Logger, cr *onapv1alpha1.CollectdPlugin) error {
+func (r *ReconcileCollectdGlobal) finalizeCollectdGlobal(reqLogger logr.Logger, cr *onapv1alpha1.CollectdGlobal) error {
 	// Cleanup by regenerating new collectd conf and rolling update of DaemonSet
-	if err := r.handleCollectdPlugin(reqLogger, cr, true); err != nil {
-		reqLogger.Error(err, "Finalize CollectdPlugin failed!!")
+	if err := r.handleCollectdGlobal(reqLogger, cr, true); err != nil {
+		reqLogger.Error(err, "Finalize CollectdGlobal failed!!")
 		return err
 	}
-	reqLogger.Info("Successfully finalized CollectdPlugin!!")
+	reqLogger.Info("Successfully finalized CollectdGlobal!!")
 	return nil
 }
 
-func (r *ReconcileCollectdPlugin) addFinalizer(reqLogger logr.Logger, cr *onapv1alpha1.CollectdPlugin) error {
-	reqLogger.Info("Adding Finalizer for the CollectdPlugin")
+func (r *ReconcileCollectdGlobal) addFinalizer(reqLogger logr.Logger, cr *onapv1alpha1.CollectdGlobal) error {
+	reqLogger.Info("Adding Finalizer for the CollectdGlobal")
 	cr.SetFinalizers(append(cr.GetFinalizers(), collectdutils.CollectdFinalizer))
 	// Update status from Initial to Created
 	// Since addFinalizer will be executed only once,
@@ -301,8 +305,20 @@ func (r *ReconcileCollectdPlugin) addFinalizer(reqLogger logr.Logger, cr *onapv1
 	// Update CR
 	err := r.client.Update(context.TODO(), cr)
 	if err != nil {
-		reqLogger.Error(err, "Failed to update CollectdPlugin with finalizer")
+		reqLogger.Error(err, "Failed to update CollectdGlobal with finalizer")
 		return err
 	}
+	return nil
+}
+
+func (r *ReconcileCollectdGlobal) handleAdditonalConfigMap(reqLogger logr.Logger, cr *onapv1alpha1.CollectdGlobal, ds *extensionsv1beta1.DaemonSet) error {
+	cm := &corev1.ConfigMap{}
+	key := types.NamespacedName{Namespace: cr.Namespace, Name: cr.Spec.ConfigMap}
+	err := r.client.Get(context.TODO(), key, cm)
+	if err != nil {
+		reqLogger.Info("Error getting TypesDB")
+		return nil
+	}
+	// TODO: Implement Types.DB mounting
 	return nil
 }
