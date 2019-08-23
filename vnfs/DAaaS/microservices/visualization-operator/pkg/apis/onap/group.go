f2d37cb632bb1706f3e223776e72e50253d4025c
Srivahni <srivahni.chivukula@intel.com>
2019-08-23 11:19:26 -0700

    visualization operator: Add datasource
    Added support to create a grafana datasource dynamically
    using GrafanaDataSource CR.
    
    Issue-ID: ONAPARC-393
    Signed-off-by: Srivahni <srivahni.chivukula@intel.com>
    Change-Id: Iaa645c5c4f303afbcec114c00e35cd6e6ae6c98c

---
+++ vnfs/DAaaS/microservices/visualization-operator/pkg/apis/onap/group.go
@@ -0,0 +1,6 @@
+// Package onap contains onap API versions.
+//
+// This file ensures Go source parsers acknowledge the onap package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package onap
