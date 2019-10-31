63d6e222a5099317fdd92fb0aa02a48e8301842b
Dileep Ranganathan <dileep.ranganathan@intel.com>
2019-10-31 15:46:37 -0700

    Prometheus Kafka Writer Microservice
    This patch implents Prometheus to remote Kafka Writer Microservice
    Added sample day-2 config to configure prometheus
    
    Issue-ID: ONAPARC-393
    Signed-off-by: Dileep Ranganathan <dileep.ranganathan@intel.com>
    Change-Id: I0bc77175593a165effd7bb1cb4802c988a5ef4ec

---
+++ vnfs/DAaaS/microservices/prom-kafka-writer/pkg/kafkawriter/kafkawriter.go
@@ -0,0 +1,123 @@
+/*
+ *
+ * Copyright 2019 Intel Corporation.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package kafkawriter
+
+import (
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	"strconv"
+	"sync"
+)
+
+//KWConfig - serialized type for config related to Kafka
+type KWConfig struct {
+	//Broker - Kafka Bootstrap servers (comma separated)
+	Broker string `json:"bootstrap.servers"`
+	//Topic - kafka topic name
+	Topic string `json:"topic"`
+	//UsePartition - Enforce use of partitions
+	UsePartition bool   `json:"usePartition"`
+	BatchMsgNum  int    `json:"batch.num.messages,omitempty"`
+	Compression  string `json:"compression.codec,omitempty"`
+}
+
+//KWProducer - holds the Kafka Config and associated Kafka Producer
+type KWProducer struct {
+	Config   KWConfig
+	Producer *kafka.Producer
+}
+
+//KWRespMap packs the KWConfig and kwid for List Api
+type KWRespMap map[string]KWConfig
+
+//KWMap - Stores the Kafka Writer to Kafka Producer Mapping
+// 		  This is used to uniquely identify a Kafka Writer - Producer mapping.
+var (
+	KWMap   = make(map[string]KWProducer)
+	kwMutex sync.Mutex
+	id      int
+)
+
+// NewKafkaWriter - creates a new producer using kafka config.
+// Handles the remote write from prometheus and send to kafka topic
+func NewKafkaWriter(kwc *KWConfig) *kafka.Producer {
+	producer, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers":   kwc.Broker,
+		"compression.codec":   kwc.Compression,
+		"batch.num.messages":  kwc.BatchMsgNum,
+		"go.batch.producer":   true,
+		"go.delivery.reports": false,
+	})
+	if err != nil {
+		return nil
+	}
+	return producer
+}
+
+//NewKWConfig - creates a KWConfig object with default values
+func NewKWConfig() *KWConfig {
+	return &KWConfig{
+		UsePartition: false,
+		BatchMsgNum:  10000,
+		Compression:  "none",
+	}
+}
+
+//NewKWRespMap - packs the KWConfig and kwid for List Api
+func newKWRespMap() KWRespMap {
+	kwr := make(KWRespMap)
+	return kwr
+}
+
+//AddKWC - Method to add KafkaWriterConfig request to KWMap
+func AddKWC(kwc *KWConfig) string {
+	kwMutex.Lock()
+	defer kwMutex.Unlock()
+	//TODO: Generate kwid
+	kwid := "pkw" + strconv.Itoa(id)
+	id++
+
+	KWMap[kwid] = KWProducer{
+		Config:   *kwc,
+		Producer: NewKafkaWriter(kwc),
+	}
+	return kwid
+}
+
+//DeleteKWC - Method to add KafkaWriter request to KWMap
+func DeleteKWC(kwid string) {
+	kwMutex.Lock()
+	defer kwMutex.Unlock()
+	if _, ok := KWMap[kwid]; ok {
+		KWMap[kwid].Producer.Close()
+	}
+	delete(KWMap, kwid)
+}
+
+//ListKWC - Method to add KafkaWriter request to KWMap
+func ListKWC() KWRespMap {
+	kwr := newKWRespMap()
+	for k, v := range KWMap {
+		kwr[k] = v.Config
+	}
+	return kwr
+}
+
+//Cleanup - Method to cleanup resources
+func Cleanup() {
+	for k := range KWMap {
+		DeleteKWC(k)
+	}
+}
---
+++ vnfs/DAaaS/microservices/prom-kafka-writer/pkg/kafkawriter/producer.go
@@ -0,0 +1,82 @@
+/*
+ *
+ * Copyright 2019 Intel Corporation.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package kafkawriter
+
+import (
+	"encoding/json"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/prompb"
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	logger "prom-kafka-writer/pkg/config"
+)
+
+var log = logger.GetLoggerInstance()
+
+func PublishTimeSeries(kwid string, metrics *prompb.WriteRequest) error {
+	log.Debugw("Remote write Time Series", "length", len(metrics.Timeseries), "TimeSeries", metrics.Timeseries)
+	for _, ts := range metrics.Timeseries {
+		m := make(model.Metric, len(ts.Labels))
+		for _, l := range ts.Labels {
+			m[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+		}
+		log.Debugw("Labels", "Labelset", m)
+
+		for _, s := range ts.Samples {
+			log.Debugf("  %f %d\n", s.Value, s.Timestamp)
+			metric := map[string]interface{}{
+				"name":      m["__name__"],
+				"labels":    m,
+				"timestamp": s.Timestamp,
+				"value":     s.Value,
+			}
+			key := string(m["__name__"])
+			jsonMetric, err := json.Marshal(metric)
+			if err != nil {
+				log.Errorw("Unmarshal error", "error", err.Error())
+			}
+			err = publish(kwid, key, jsonMetric)
+			if err != nil {
+				log.Error("Failed to produce message")
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func publish(kwid string, key string, jsonMetric []byte) error {
+	var (
+		kwp = KWMap[kwid].Producer
+		kwc = KWMap[kwid].Config
+	)
+
+	tp := getTopicPartition(kwc)
+	kwMsg := kafka.Message{TopicPartition: tp, Key: []byte(key), Value: jsonMetric}
+	err := kwp.Produce(&kwMsg, nil)
+	if err != nil {
+		log.Errorw("Kafka Producer Error", "error", err.Error())
+	}
+	return err
+}
+
+func getTopicPartition(kwc KWConfig) kafka.TopicPartition {
+	p := kafka.PartitionAny
+	if kwc.UsePartition {
+		// TODO: Implement partition strategy
+		p = kafka.PartitionAny
+	}
+	return kafka.TopicPartition{Topic: &kwc.Topic, Partition: p}
+}
