68d118176bb53c36b31a7060cfa16ad5acac1765
Rajamohan Raj <rajamohan.raj@intel.com>
2019-10-31 23:51:29 +0000

    HDFS-WriterApp-Fixed all the code review comments
    Fixed all the code review comments by Kiran
    
    	1. Implemented a boolean channel instead of empty struct channel for signal, use WaitGroup
    	to ensure all writers finish cleanup.
    	2. Introduce JSON tags for configs
    	4. remove all panic and fatalf code to ensure that the app doesnt crash anytime.
    	5. remove unneccessary hdfsWriter null checks.
            6.remove the 'run' variable used in the infinite loop, replaced with 'return'
    
    Issue-ID: ONAPARC-453
    Change-Id: Ic77c59dc75a8898a3cf34999850e6687d40e7faa
    Signed-off-by: Rajamohan Raj <rajamohan.raj@intel.com>

--- vnfs/DAaaS/microservices/GoApps/src/go-hdfs-writer/cmd/hdfs-writer/main.go
+++ vnfs/DAaaS/microservices/GoApps/src/go-hdfs-writer/cmd/hdfs-writer/main.go
@@ -6,9 +6,9 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"time"
 
 	handler "hdfs-writer/pkg/handler"
+	pipeline "hdfs-writer/pkg/pipeline"
 	utils "hdfs-writer/pkg/utils"
 )
 
@@ -22,23 +22,24 @@ func main() {
 		Addr:    ":9393",
 	}
 
-	connectionsClose := make(chan struct{})
+	connectionsClose := make(chan bool)
 	go func() {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt)
 		<-c // function literal waiting to receive Interrupt signal
 		fmt.Printf(":::Got the kill signal:::")
 		slogger.Info(":::Got the kill signal:::")
-		for eachWriter, eachChannel := range handler.ChannelMap {
+		for eachWriter, eachChannel := range pipeline.ChannelMap {
+			slogger.Info("Begin:: Closing writer goroutines::")
 			slogger.Infof("Closing writer goroutine :: %s", eachWriter)
-			slogger.Infof("eachChannel:: %v", eachChannel)
-			close(eachChannel)
-			// This wait time ensures that the each of the channel is killed before
-			// main routine finishes.
-			time.Sleep(time.Second * 5)
+			delete(pipeline.ChannelMap, eachWriter)
+			eachChannel <- true
 		}
-		//once all goroutines are signalled, send close to main thread
+
 		httpServer.Shutdown(context.Background())
+		/*once all goroutines are signalled and httpServer is shutdown,
+		send close to main thread */
+		connectionsClose <- true
 		close(connectionsClose)
 	}()
 
@@ -47,5 +48,6 @@ func main() {
 	if err != nil && err != http.ErrServerClosed {
 		slogger.Fatal(err)
 	}
+	pipeline.Wg.Wait()
 	<-connectionsClose //main thread waiting to receive close signal
 }
--- vnfs/DAaaS/microservices/GoApps/src/go-hdfs-writer/pkg/utils/hdfsUtils.go
+++ vnfs/DAaaS/microservices/GoApps/src/go-hdfs-writer/pkg/utils/hdfsUtils.go
@@ -3,23 +3,8 @@ package utils
 import (
 	"fmt"
 	"github.com/colinmarc/hdfs"
-	//"sync"
-	//"go.uber.org/zap"
 )
 
-
-//var clientOnce sync.Once
-//var hdfsClient *hdfs.Client
-//var slogger *zap.SugaredLogger
-
-
-//GetHdfsClientInstance returns a singleton hdfsClient instance
-// func GetHdfsClientInstance(hdfsURL string) (*hdfs.Client){
-// 	clientOnce.Do(func(){
-// 		hdfsClient = createHdfsClient(hdfsURL)
-// 	})
-// 	return hdfsClient
-// }
 
 //CreateHdfsClient creates a hdfs client and returns hdfs client
 func CreateHdfsClient(hdfsURL string) (*hdfs.Client){
