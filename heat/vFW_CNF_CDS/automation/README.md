fa4a48129af120989198778673d4d88b8d444881
Michal Chabiera <michal.chabiera@orange.com>
2020-12-07 18:18:27 +0100

    Automation scripts for vFW_CNF_CDS usecase
    Automation scripts for vFW_CNF_CDS usecase
    
    Issue-ID: INT-1658
    Signed-off-by: Michal Chabiera <michal.chabiera@orange.com>
    Change-Id: I32ba9afe0ca95c5db66c6789756508a76a13f948

---
+++ heat/vFW_CNF_CDS/automation/README.md
@@ -0,0 +1,16 @@
+1. Install required packages with pipenv `pipenv install`
+2. Run virtual environment `pipenv shell --fancy`
+3. Add kubeconfig files, one for ONAP cluster, and one for k8s cluster that will host vFW:
+   - `artifacts/cluster_kubeconfig`
+   - `artifacts/onap_kubeconfig`
+4. Prepare onboarding packages `cd ../templates/ && make && cd ../automation/`
+5. Modify `config.py`:
+   - NATIVE - enables native helm orchestration path in SO
+   - CLOUD_REGION
+   - GLOBAL_CUSTOMER_ID
+   - VENDOR
+   - SERVICENAME
+   - CUSTOMER_RESOURCE_DEFINITIONS - add list of CRDs to be installed on non KUD k8s cluster
+6. Run script `python create_k8s_region.py` in order to create **k8s cloud region**   
+7. Onboard **vFW** `python onboard.py`
+8. Instantiate **vFW** `python instantiate.py`
---
+++ heat/vFW_CNF_CDS/automation/delete.py
@@ -0,0 +1,61 @@
+# ============LICENSE_START=======================================================
+# Copyright (C) 2020 Orange
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ============LICENSE_END=========================================================
+
+import logging
+
+from onapsdk.aai.business import Customer
+
+from config import Config
+
+logger = logging.getLogger("")
+logger.setLevel(logging.DEBUG)
+fh = logging.StreamHandler()
+fh_formatter = logging.Formatter('%(asctime)s %(levelname)s %(lineno)d:%(filename)s(%(process)d) - %(message)s')
+fh.setFormatter(fh_formatter)
+logger.addHandler(fh)
+
+logger.info("******** Get Customer *******")
+customer = None
+try:
+    customer = Customer.get_by_global_customer_id(Config.GLOBAL_CUSTOMER_ID)
+except:
+    logger.error("Customer not found")
+    exit(1)
+
+logger.info("******** Check Service Subscription *******")
+service_subscription = None
+for service_sub in customer.service_subscriptions:
+    if service_sub.service_type == Config.SERVICENAME:
+        logger.info("Service %s subscribed", Config.SERVICENAME)
+        service_subscription = service_sub
+        break
+if not service_subscription:
+    logger.error("Service Subscription not found")
+    exit(1)
+
+logger.info("******** Get Service Instance details *******")
+service_instance = None
+for service in service_subscription.service_instances:
+    if service.instance_name == Config.SERVICE_INSTANCE_NAME:
+        service_instance = service
+        break
+if not service_instance:
+    logger.error("Service Instance not found")
+    exit(1)
+
+logger.info("******** Delete Service %s *******", service_instance.instance_name)
+service_deletion = service_instance.delete()
