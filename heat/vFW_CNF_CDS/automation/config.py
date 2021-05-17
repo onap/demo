77e4a26ddb4466feb2558d16e73edacbc61b5a0a
Konrad Bańka <k.banka@samsung.com>
2021-05-17 12:31:15 +0200

    [vFW_CNF_CDS] Add Healthcheck automation
    Provide dedicated script to invoke CDS Healthcheck flow.
    
    Issue-ID: INT-1899
    Signed-off-by: Konrad Bańka <k.banka@samsung.com>
    Change-Id: I4d61edcc7609dbc9550a97c278531f2a940a85bc

--- heat/vFW_CNF_CDS/automation/config.py
+++ heat/vFW_CNF_CDS/automation/config.py
@@ -1,5 +1,6 @@
 # ============LICENSE_START=======================================================
 # Copyright (C) 2021 Orange
+# Copyright (C) 2021 Samsung, Modifications
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
