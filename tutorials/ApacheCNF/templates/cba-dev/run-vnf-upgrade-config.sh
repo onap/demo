5e393641f0320a69f5fc699e38993b6d017accf8
Lukasz Rajewski <lukasz.rajewski@t-mobile.pl>
2022-11-14 14:48:24 +0100

    [APACHECNF] CDS workflows for upgrade scenario
    Issue-ID: INT-2164
    Signed-off-by: Lukasz Rajewski <lukasz.rajewski@t-mobile.pl>
    Change-Id: Icfa9dd7d90abb3da2b0cf388cfb1e3a27e8edd6d
    (cherry picked from commit 4193053d0fc30b688f991e2356e2c0cd6c446a57)

--- tutorials/ApacheCNF/templates/cba-dev/run-vnf-upgrade-config.sh
+++ tutorials/ApacheCNF/templates/cba-dev/run-vnf-upgrade-config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # ============LICENSE_START=======================================================
-# Copyright (C) 2020 Orange
+# Copyright (C) 2022 Deutche Telekom AG
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@
 
 REQ_ID=`shuf -i 1-1000000 -n 1`
 SUB_REQ_ID=$REQ_ID"-"`shuf -i 1-1000 -n 1`
+ACTION=$1
 
 curl --location --request POST 'http://localhost:8081/api/v1/execution-service/process' \
 --header 'Authorization: Basic Y2NzZGthcHBzOmNjc2RrYXBwcw==' \
@@ -32,26 +33,20 @@ curl --location --request POST 'http://localhost:8081/api/v1/execution-service/p
     "actionIdentifiers": {
         "blueprintName": "APACHE",
         "blueprintVersion": "1.0.0",
-        "actionName": "resource-assignment",
+        "actionName": "'config-upgrade-$ACTION'",
         "mode": "sync"
     },
     "payload": {
-        "resource-assignment-request": {
-            "template-prefix": [
-                "vnf"
-            ],
-            "resolution-key": "ra-test-resolution",
-            "resource-assignment-properties": {
-                "replica-count": 2,
-                "aic-cloud-region": "RegionOne",
-                "vnf-model-customization-uuid": "d73864db-1f6e-4e54-a533-a96773c926a4",
-                "service-instance-id": "2afee7c4-8b16-4f2f-a567-48fb7948abcf",
-                "vnf-id": "51274ece-55ca-4cbc-b7c4-0da0dcc65d38",
-                "vnf_name": "sample-vnf-name",
-                "k8s-rb-profile-namespace": "vfw-namespace",
-                "k8s-rb-profile-k8s-version": "1.18.9"
-            }
+       "'config-upgrade-$ACTION-request'": {
+           "resolution-key": "VF_apache_k8s_demo_CNF",
+           "'config-upgrade-$ACTION-properties'": {
+               "service-instance-id": "5e6c49cc-e569-4eb9-bc9e-f7ce0ffb798f",
+               "service-model-uuid": "0997c40e-63b7-419d-a13b-6a5ce97334bb",
+               "vnf-id": "935fe87e-9bbd-4828-add2-d90196f5f382",
+               "vnf-name": "Instance_apache-3_0",
+               "vnf-customization-uuid": "6b6ff775-a170-4ce4-bcd0-85645d738390"
+           }
         }
     }
-}' | jq '.payload | .["resource-assignment-response"] | .["meshed-template"] | .vnf | fromjson | .["resource-accumulator-resolved-data"] '
+}' | jq '.payload | .["'config-upgrade-$ACTION-response'"]'
 
